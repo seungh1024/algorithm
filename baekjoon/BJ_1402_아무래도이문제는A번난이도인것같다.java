866970e0074758828f80083837212805cce4b186
seungh <seungh1024@naver.com>
2023-07-01 17:19:35 +0900

    알고리즘 풀이 추가

---
+++ baekjoon/BJ_1402_아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,13 @@
+package algo_202304;
+
+import java.io.*;
+
+public class BJ_1402_아무래도이문제는A번난이도인것같다 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            System.out.println("yes");
+        }
+    }
+}

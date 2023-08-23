e4345f6d2bb96dba4d245e77b3cd2dbabd43a2bb
seungh <seungh1024@naver.com>
2023-08-23 20:09:12 +0900

    알고리즘 풀이 추가

---
+++ baekjoon/BJ_15549_if.java
@@ -0,0 +1,23 @@
+package algo_202308;
+
+import java.io.*;
+
+public class BJ_15549_if {
+    public static void main(String[] args) throws IOException{
+
+//        int num = Integer.MAX_VALUE+1;
+//        if(num != 0 && num == -num){
+//            System.out.println("true");
+//        }else{
+//            System.out.println("false");
+//        }
+////        System.out.println(num);
+        int num = Integer.MAX_VALUE+1;
+//        System.out.println(-num);
+        System.out.println("int "+ num);
+//        String val = ""+num;
+//        int temp = Integer.parseInt(val);
+//        System.out.println(temp);
+//        System.out.println(-temp);
+    }
+}

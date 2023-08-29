4df86c435e4a4097001bd803274552389e8a092f
seungh <seungh1024@naver.com>
2023-08-29 17:52:37 +0900

    알고리즘 풀이 추가

---
+++ baekjoon/BJ_27465_소수가아닌수.java
@@ -0,0 +1,12 @@
+package algo_202308;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_27465_소수가아닌수 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(1000000000);
+    }
+}

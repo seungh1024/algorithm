16cfa3c52d52803832af6788b9294a6d43d576ef
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-01-22 01:38:33 +0900

    [Gold IV] Title: 도둑, Time: 548 ms, Memory: 96812 KB -BaekjoonHub

---
+++ 백준/Gold/13422. 도둑/도둑.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+
+			int[] data = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				data[i] = Integer.parseInt(st.nextToken());
+			}
+
+			int left = 0;
+			int right = 0;
+			int sum = 0;
+			for (int i = 0; i < M; i++) {
+				sum += data[right];
+				right = (right+1)%N;
+			}
+
+			if (N == M) {
+				if (sum < K) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
+				}
+				continue;
+			}
+
+			int result = 0;
+			for (; left < N; left++) {
+				if (sum < K) {
+					result++;
+				}
+				// System.out.println("sum = " + sum +", left = "+left +", right = "+right);
+				sum -= data[left];
+				sum += data[right];
+				right = (right+1)%N;
+			}
+			sb.append(result).append("\n");
+
+		}
+
+		System.out.println(sb);
+	}
+}

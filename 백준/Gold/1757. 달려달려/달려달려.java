43d46561af2071e35c3daa74ac24939f72cf3a52
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-01-22 18:26:47 +0900

    [Gold IV] Title: 달려달려, Time: 260 ms, Memory: 39428 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.java
+++ 백준/Gold/1757. 달려달려/달려달려.java
@@ -4,44 +4,45 @@ import java.util.*;
 public class Main {
 	public static int N, M;
 	public static int[] data;
-	public static int[][][] dp;
+	public static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		data = new int[N];
-		for (int i = 0; i < N; i++) {
+
+		data = new int[N + 1];
+		dp = new int[N + 1][M + 1];
+
+		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(br.readLine());
 		}
 
-		dp = new int[N][M+1][2];
 
-		dp[0][1][1] = data[0];
-
-		for (int i = 1; i < N; i++) {
-			for (int j = 0; j <=M; j++) {
-				if (j > 0) {
-					if (j+1 <= M) {
-						dp[i][j][0] = Math.max(dp[i][j][0], Math.max(dp[i - 1][j + 1][1], dp[i - 1][j + 1][0]));
-					}
-					if (j - 1 == 0) {
-						dp[i][j][1] = Math.max(dp[i][j][1], dp[i - 1][0][0] + data[i]);
-					}
-					dp[i][j][1] = Math.max(dp[i][j][1], dp[i - 1][j - 1][1] + data[i]);
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j <= M; j++) {
+				if (j == 0) {
+					dp[i][j] = dp[i - 1][j];
 				} else {
-					// System.out.println("i = "+i +", j = "+j);
-					dp[i][j][0] = Math.max(dp[i][j][0], dp[i - 1][j][0]);
-					if (j + 1 <= M) {
-						dp[i][j][0] = Math.max(dp[i][j][0], Math.max(dp[i - 1][j + 1][1], dp[i - 1][j + 1][0]));
-					}
+					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j - 1] + data[i]);
 				}
 			}
+			int max = 0;
+			for (int j = 1; j <= M; j++) {
+				if (i - j >=0) {
+					max = Math.max(max, dp[i - j][j]);
+				}
+			}
+			dp[i][0] = Math.max(dp[i][0], max);
 		}
 
-		System.out.println(dp[N-1][0][0]);
-		// System.out.println(dp[N-1][0][1]);
+		System.out.println(dp[N][0]);
+
+		// for (int i = 0; i <= N; i++) {
+		// 	System.out.println(Arrays.toString(dp[i]));
+		// }
 	}
+
 
 }

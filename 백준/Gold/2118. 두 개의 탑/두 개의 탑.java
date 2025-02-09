ce5fce2d52176424a498cc99a9b278e525f77b2f
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-09 16:18:32 +0900

    [Gold V] Title: 두 개의 탑, Time: 200 ms, Memory: 20700 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -6,47 +6,48 @@ public class Main {
 	public static int[] data;
 	public static int[] sum;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N+1];
 		sum = new int[N+1];
-
 		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(br.readLine());
-			sum[i] = sum[i-1] + data[i];
+			sum[i] += data[i] + sum[i - 1];
 		}
+		// System.out.println(Arrays.toString(sum));
 
 		int left = 0;
 		int right = 1;
-		int now = 0;
-		int total = sum[N];
-		int max = now;
-		int count = 0;
+		int range = 0;
+		int max = 0;
 		while (true) {
 			int rightValue = Math.abs(sum[right] - sum[left]);
-			int leftValue = total - Math.abs((sum[right] - sum[left]));
+			int leftValue = sum[N] - rightValue;
+
 			if (rightValue <= leftValue) {
-				max = Math.max(max, rightValue);
 				right++;
-			}else{
-				left++;
+				max = Math.max(max, rightValue);
+			} else {
+				left ++;
 				max = Math.max(max, leftValue);
 			}
 
 			if (right > N) {
 				right %= N;
-				count++;
+				range++;
 			}
 			if (left > N) {
 				left %= N;
 			}
-			if (count >= 2) {
+			if (range > 2) {
 				break;
 			}
 		}
-
 		System.out.println(max);
 
+
 	}
+
+
 }

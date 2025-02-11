016d78f87fc1be1c1d6a8c61e4eb0dacfd2e2e1c
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-11 15:28:39 +0900

    [Gold V] Title: 배, Time: 144 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -2,11 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
+	public static int N, M;
 	public static int[] crane;
-	public static int M;
 	public static int[] box;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -18,62 +16,69 @@ public class Main {
 		M = Integer.parseInt(br.readLine());
 		box = new int[M];
 		st = new StringTokenizer(br.readLine());
+
 		for (int i = 0; i < M; i++) {
 			box[i] = Integer.parseInt(st.nextToken());
 		}
 
 		Arrays.sort(crane);
 		Arrays.sort(box);
+
 		// System.out.println(Arrays.toString(crane));
 		// System.out.println(Arrays.toString(box));
-		binarySearch();
+		int[] count = new int[N];
+		int last = 0;
+		for (int i = 0; i < N; i++) {
+			int idx = find(last, crane[i]);
+			count[i] = idx - last;
+			last = idx;
+		}
+		// System.out.println(Arrays.toString(count));
+
+		for (int i = N - 2; i >= 0; i--) {
+
+			while (true) {
+				int min = Integer.MAX_VALUE;
+				int idx = 0;
+				for (int j = i + 1; j < N; j++) {
+					if (count[j] < min) {
+						min = count[j];
+						idx = j;
+					}
+				}
+				if(count[i] - min <= 1) break;
+
+				count[idx]++;
+				count[i]--;
+			}
+		}
+
+		// System.out.println(Arrays.toString(count));
+		int max = 0;
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, count[i]);
+			cnt += count[i];
+		}
+		if (cnt != M) {
+			max = -1;
+		}
+		System.out.println(max);
 	}
 
-	public static void binarySearch() {
-		long start = 0;
-		long end = Integer.MAX_VALUE-1;
+	public static int find(int start, int target) {
 
+		int end = M;
 		while (start < end) {
-			long mid = (start+end)/2;
-			// System.out.println(mid);
+			int mid = (start + end) / 2;
 
-			boolean result = makeResult(mid);
-
-			// System.out.println(result);
-
-			if (result) {
+			if (box[mid] > target) {
 				end = mid;
 			} else {
-				start = mid+1;
+				start = mid + 1;
 			}
 		}
 
-		if (makeResult(start)) {
-			System.out.println(start);
-		} else {
-			System.out.println(-1);
-		}
-	}
-
-	public static boolean makeResult(long num) {
-		int craneIdx = N-1;
-		int boxIdx = M-1;
-
-		// System.out.println("======= num = "+num);
-		while (craneIdx >= 0 && boxIdx >= 0) {
-			// System.out.println("craneIdx = "+craneIdx + ", boxIdx = "+boxIdx);
-			// System.out.println("crane = "+crane[craneIdx] + ", box = "+box[boxIdx]);
-			if (crane[craneIdx] < box[boxIdx]) {
-				return false;
-			}
-			craneIdx--;
-			boxIdx-=num;
-		}
-
-		if (boxIdx >= 0 && craneIdx < 0) {
-			return false;
-		}
-
-		return true;
+		return start;
 	}
 }

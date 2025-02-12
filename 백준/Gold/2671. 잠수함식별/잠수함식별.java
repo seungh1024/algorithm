5fd8cb44698d8abc7129ed597c543b72af2cfeae
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-12 17:40:45 +0900

    [Gold V] Title: 잠수함식별, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] data = br.readLine().toCharArray();
+		int N = data.length;
+		int idx = 0;
+
+		boolean flag = false;
+		while (idx < N) {
+			if (data[idx] == '1') {
+				// System.out.println("1" + ", idx = "+idx);
+				if (idx + 2 > N) {
+					flag = true;
+					break;
+				}
+
+				int cnt = 0;
+				idx++;
+				boolean check1 = false;
+				boolean check2 = false;
+				while (idx < N && data[idx] == '0') {
+					idx++;
+					cnt++;
+					check1 = true;
+				}
+				if (cnt < 2) {
+					flag = true;
+					break;
+				}
+				while (idx < N && data[idx] == '1') {
+					if (check2 && idx + 2 < N && data[idx + 1] == '0' && data[idx+2] == '0') {
+						// idx++;
+						break;
+					}
+					check2 = true;
+					idx++;
+				}
+				if (!check1 || !check2) {
+					flag = true;
+					break;
+				}
+			} else {
+				// System.out.println("0" +", idx = "+idx);
+				if (idx + 1 < N && data[idx + 1] == '1') {
+					idx += 2;
+				} else {
+					flag = true;
+					break;
+				}
+			}
+		}
+
+		if (!flag) {
+			System.out.println("SUBMARINE");
+		} else {
+			System.out.println("NOISE");
+		}
+	}
+}

95314397b98b5ba05286737e27c83ba2524c913a
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-16 18:49:12 +0900

    [Gold IV] Title: 키 순서, Time: 652 ms, Memory: 49084 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class Main {
 	public static int N, M;
-	public static int[] count;
-	public static int[] parent;
-	public static int[] child;
+	public static List<Integer>[] up,down;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,76 +11,50 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
-		List<Integer>[] list = new ArrayList[N + 1];
-		List<Integer>[] list2 = new ArrayList[N + 1];
+		up = new ArrayList[N + 1];
+		down = new ArrayList[N + 1];
 		for (int i = 1; i <= N; i++) {
-			list[i] = new ArrayList<>();
-			list2[i] = new ArrayList<>();
+			up[i] = new ArrayList<>();
+			down[i] = new ArrayList<>();
 		}
-
-		count = new int[N + 1];
-		parent = new int[N + 1];
-		child = new int[N + 1];
-
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			list[from].add(to);
-			list2[to].add(from);
-			count[to]++;
-		}
-
-		int cnt = 0;
-		for (int i = 1; i <= N; i++) {
-			if (count[i] == 0) {
-				cnt++;
-			}
+			up[from].add(to);
+			down[to].add(from);
 		}
 
 		int result = 0;
-
-
 		for (int i = 1; i <= N; i++) {
-			find(i,parent,list);
-			find(i,child,list2);
-
-		}
-		for (int i = 1; i <= N; i++) {
-			if (parent[i]  + child[i] == N-1) {
-				result ++;
+			int cnt = find(i, up);
+			cnt += find(i, down);
+			if (cnt == N - 1) {
+				result++;
 			}
-
 		}
-		// System.out.println(Arrays.toString(parent));
-		// System.out.println(Arrays.toString(child));
-
 		System.out.println(result);
-
 	}
 
-	public static void find(int start, int[] arr, List<Integer>[] list) {
-
+	public static int find(int i, List<Integer>[] list) {
 		Queue<Integer> q = new ArrayDeque<>();
-		q.offer(start);
+		q.offer(i);
 		boolean[] visited = new boolean[N + 1];
-		visited[start] = true;
+		visited[i] = true;
 
+		int cnt = 0;
 		while (!q.isEmpty()) {
-			int now = q.poll();
+			int now = q.poll();;
 
 			for (int next : list[now]) {
 				if (!visited[next]) {
 					visited[next] = true;
 					q.offer(next);
-					arr[next]++;
+					cnt++;
 				}
 			}
 		}
 
-
-
+		return cnt;
 	}
-
-
 }

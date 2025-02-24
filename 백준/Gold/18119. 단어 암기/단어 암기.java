b4556e758d2513707535c6fcb9af87e7405218bc
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-24 18:23:28 +0900

    [Gold IV] Title: 단어 암기, Time: 1200 ms, Memory: 60892 KB -BaekjoonHub

--- 백준/Gold/18119. 단어 암기/단어 암기.java
+++ 백준/Gold/18119. 단어 암기/단어 암기.java
@@ -4,53 +4,52 @@ import java.util.*;
 public class Main {
 	public static int N, M;
 	public static long[] data;
-
-	public static long memory;
+	public static long mem;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+
 		data = new long[N];
-
-		for (int i = 0; i < 28; i++) {
-			memory |= 1<<i;
-		}
-
 		for (int i = 0; i < N; i++) {
 			char[] input = br.readLine().toCharArray();
-			for (int j = 0; j < input.length; j++) {
-				int idx = (int)(input[j] - 'a');
-				data[i] |= 1<<idx;
+			for (char c : input) {
+				data[i] |= 1 << (c - 'a');
 			}
 		}
 
+		mem = 0L;
+		char a = 'a';
+		for (int i = 0; i < 26; i++) {
+			mem |= 1 << (a - 'a');
+			a = (char)(a + 1);
+		}
+		// System.out.println(mem);
 
 		StringBuilder sb = new StringBuilder();
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
-			int num = Integer.parseInt(st.nextToken());
-			int idx = st.nextToken().toCharArray()[0] - 'a';
+			int cmd = Integer.parseInt(st.nextToken());
+			char c = st.nextToken().charAt(0);
 
-			if (num == 1) {
-				memory ^= 1<<idx;
+			if (cmd == 1) {
+				mem ^= 1 << (c - 'a');
 			} else {
-				memory |= 1<<idx;
+				mem |= 1 << (c - 'a');
 			}
 
 			int cnt = 0;
+
 			for (int i = 0; i < N; i++) {
-				long value = memory & data[i];
-				if (value == data[i]) {
+				if (data[i] == (mem & data[i])) {
 					cnt++;
 				}
 			}
 			sb.append(cnt).append("\n");
-
 		}
 
 		System.out.println(sb);
-
 	}
 }

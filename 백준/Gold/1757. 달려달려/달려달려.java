701f4d091fa03914004bc4c231e216fa3e9e2946
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-02-28 23:41:42 +0900

    [Gold IV] Title: 달려달려, Time: 212 ms, Memory: 35856 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.java
+++ 백준/Gold/1757. 달려달려/달려달려.java
@@ -2,35 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N, M;
-	public static int[] data;
-	public static int[][] dp;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		data = new int[N + 1];
-		dp = new int[N + 1][M + 1];
-
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] data = new int[N+1];
 		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(br.readLine());
 		}
 
-
+		int[][] dp = new int[N+1][M+1];
+		dp[1][1] = data[1];
 		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j <= M; j++) {
-				if (j == 0) {
-					dp[i][j] = dp[i - 1][j];
-				} else {
-					dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j - 1] + data[i]);
-				}
+			dp[i][0] = dp[i - 1][0];
+			for (int j = 1; j <= M; j++) {
+				dp[i][j] = Math.max(dp[i][j-1], dp[i - 1][j - 1] + data[i]);
+
 			}
+
 			int max = 0;
 			for (int j = 1; j <= M; j++) {
-				if (i - j >=0) {
+				if (i - j >= 0) {
 					max = Math.max(max, dp[i - j][j]);
 				}
 			}
@@ -39,10 +32,9 @@ public class Main {
 
 		System.out.println(dp[N][0]);
 
-		// for (int i = 0; i <= N; i++) {
+		// for (int i = 1; i <= N; i++) {
+		//
 		// 	System.out.println(Arrays.toString(dp[i]));
 		// }
 	}
-
-
 }

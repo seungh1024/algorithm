7bd7febbedb7570c9531b6a85d75f0bb921b09fe
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-06 22:58:10 +0900

    [Gold IV] Title: 문자열 제거, Time: 136 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Gold/21941. 문자열 제거/문자열 제거.java
+++ 백준/Gold/21941. 문자열 제거/문자열 제거.java
@@ -2,43 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int N;
+	public static Data[] data;
+	public static String input;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String data = br.readLine();
-		int M = Integer.parseInt(br.readLine());
-		List<Data> list = new ArrayList<>();
-		for (int i = 0; i < M; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String input = st.nextToken();
-			int cost = Integer.parseInt(st.nextToken());
+		input = br.readLine();
+		N = Integer.parseInt(br.readLine());
 
-			list.add(new Data(input, cost));
+		data = new Data[N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			int p = Integer.parseInt(st.nextToken());
+			data[i] = new Data(s, p);
 		}
 
-		int N = data.length();
-		int[] dp = new int[N+1];
-		for (int i = 1; i <= N; i++) {
-			for (Data d : list) {
-				
-				dp[i] = Math.max(dp[i], dp[i - 1]+1);
-				if (data.startsWith(d.s, i-1)) {
-					// System.out.println("?");
-					// System.out.println("i = "+i +", string = "+d.s);
-					dp[i + d.s.length()-1] = Math.max(dp[i + d.s.length()-1], dp[i-1] + d.cost);
+		int size = input.length();
+		int[] dp = new int[size+ 1];
+
+		for (int i = 1; i <= size; i++) {
+			for (Data d : data) {
+				dp[i] = Math.max(dp[i], dp[i - 1] + 1);
+				if (input.startsWith(d.s, i - 1)) {
+					dp[i + d.s.length() - 1] = Math.max(dp[i + d.s.length() - 1], dp[i - 1] + d.p);
 				}
 			}
 		}
+
 		// System.out.println(Arrays.toString(dp));
-		System.out.println(dp[N]);
+		System.out.println(dp[size]);
 	}
 
 	public static class Data{
 		String s;
-		int cost;
+		int p;
 
-		public Data(String s, int cost) {
+		public Data(String s, int p) {
 			this.s = s;
-			this.cost = cost;
+			this.p = p;
 		}
 	}
 }

2fb94e69b305f8ecc9588a6a3715f102be55f55d
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-13 16:31:17 +0900

    [Gold II] Title: 성냥개비, Time: 104 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.java
+++ 백준/Gold/3687. 성냥개비/성냥개비.java
@@ -1,41 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static int[] big = {-1, -1, 1, 7, 4, 2, 0, 8,10};
+	public static int[] data = {-1, -1, 1, 7, 4, 2, 0, 8, 10};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+
 		long[] dp = new long[101];
 		for (int i = 2; i < 9; i++) {
-			dp[i] = big[i];
+			dp[i] = data[i];
 		}
 		dp[6] = 6;
-		
+
 		for (int i = 9; i <= 100; i++) {
-			dp[i] = dp[i - 2] * 10 + big[2];
+			dp[i] = dp[i - 2] * 10 + data[2];
 			for (int j = 2; j < 8; j++) {
-				dp[i] = Math.min(dp[i], dp[i - j] * 10 + big[j]);
+				dp[i] = Math.min(dp[i], dp[i - j] * 10 + data[j]);
 			}
 		}
 
 		for (int t = 0; t < T; t++) {
 			int N = Integer.parseInt(br.readLine());
 
-			sb.append(dp[N]);
-			sb.append(" ");
+			sb.append(dp[N]).append(" ");
 
 			StringBuilder max = new StringBuilder();
-			int size = N/2;
-			for (int i = 1; i < size; i++) {
-				max.append("1");
+			while (N > 0) {
+				if (N == 3) {
+					sb.append(7);
+					break;
+				}
+				max.append(1);
+				N -= 2;
 			}
-			if (N % 2 == 0) {
-				sb.append(1).append(max);
-			} else {
-				sb.append(7).append(max);
-			}
-			sb.append("\n");
+			sb.append(max).append("\n");
 		}
 		System.out.println(sb);
 	}

5ed65457e56bc37c2be1c8f9f04d915b41121d69
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-14 17:45:37 +0900

    [Gold III] Title: 탑 보기, Time: 544 ms, Memory: 38500 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.java
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -4,93 +4,81 @@ import java.util.*;
 public class Main {
 	public static int N;
 	public static int[] data;
-	public static int[] count;
-	public static int[] max;
-	public static int size = 100001;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N];
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			data[i]	= Integer.parseInt(st.nextToken());
-
+			data[i] = Integer.parseInt(st.nextToken());
 		}
-		count = new int[size];
-		max = new int[size];
 
-		Stack<Integer> left = new Stack<>();
-		left.push(0);
-		boolean[] flag = new boolean[size];
+		Stack<int[]> stack = new Stack<>();
+		int[] left = new int[N];
+		int[] leftIdx = new int[N];
+		stack.push(new int[] {0, data[0]});
 		for (int i = 1; i < N; i++) {
-			int now = data[i];
-
-			while (!left.isEmpty()) {
-				int idx = left.pop();
-				int small = data[idx];
-				if (small > now) {
-					left.push(idx);
-					break;
-				}
+			while (!stack.isEmpty() && stack.peek()[1] <= data[i]) {
+				stack.pop();
 			}
-
-			// System.out.println("left = "+left);
-			if (left.size() > 0) {
-				count[i] += left.size();
-				max[i] = left.peek();
-				flag[i] = true;
+			left[i] = stack.size();
+			if (!stack.isEmpty()) {
+				leftIdx[i] = stack.peek()[0]+1;
 			}
-			left.push(i);
-		}
-		// for (int i = 0; i < N; i++) {
-		// 	System.out.println(max[i]);
-		// }
-
-		Stack<Integer> right = new Stack<>();
-		right.push(N-1);
-		for (int i = N - 2; i >= 0; i--) {
-			int now = data[i];
-
-			while (!right.isEmpty()) {
-				int idx = right.pop();
-				int small = data[idx];
-				if (small > now) {
-					right.push(idx);
-					break;
-				}
-			}
-
-			if (right.size() > 0) {
-				count[i] += right.size();
-				if (!flag[i]) {
-					max[i] = right.peek();
-				} else {
-					int a = Math.abs(i - max[i]);
-					int b = Math.abs(i-right.peek());
-					if (b < a) {
-						max[i] = right.peek();
-					}
-				}
-			}
-			right.push(i);
+			stack.push(new int[] {i, data[i]});
 		}
 
-		// System.out.println("====");
-		// for (int i = 0; i < N; i++) {
-		// 	System.out.println(max[i] + ", count = "+count[i]);
-		// }
-		// System.out.println("====");
+		// System.out.println(Arrays.toString(left));
+
+		int[] right = new int[N];
+		int[] rightIdx = new int[N];
+		stack = new Stack<>();
+		stack.push(new int[] {N - 1, data[N - 1]});
+		for (int i = N-1; i >= 0; i--) {
+			while (!stack.isEmpty() && stack.peek()[1] <= data[i]) {
+				stack.pop();
+			}
+
+			right[i] = stack.size();
+			if (!stack.isEmpty()) {
+				rightIdx[i] = stack.peek()[0]+1;
+			}
+			stack.push(new int[] {i, data[i]});
+		}
+
+		// System.out.println(Arrays.toString(right));
+		int[] idx = new int[N];
+		for (int i = 0; i < N; i++) {
+			boolean flag = false;
+			if (leftIdx[i] == 0 && rightIdx[i] != 0) {
+				idx[i] = rightIdx[i];
+				flag = true;
+			} else if (leftIdx[i] != 0 && rightIdx[i] == 0) {
+				idx[i] = leftIdx[i];
+				flag = true;
+			}
+
+			if(flag) continue;
+
+
+		 	if (Math.abs(leftIdx[i] - i-1) <= Math.abs(rightIdx[i] - i-1)) {
+				idx[i] = leftIdx[i];
+			} else {
+				idx[i] = rightIdx[i];
+			}
+		}
+		// System.out.println(Arrays.toString(leftIdx));
+		// System.out.println(Arrays.toString(rightIdx));
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			if (count[i] > 0) {
-				sb.append(count[i]).append(" ").append(max[i]+1).append("\n");
+			if (left[i] + right[i] == 0) {
+				sb.append(0);
 			} else {
-				sb.append(0).append("\n");
+				sb.append(left[i] + right[i]).append(" ").append(idx[i]);
 			}
+			sb.append("\n");
 		}
-
 		System.out.println(sb);
 	}
 }

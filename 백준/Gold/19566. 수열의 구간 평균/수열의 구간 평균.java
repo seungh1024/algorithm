05c26429c4d98899c8dc59361e6fafdf4c78184b
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-19 01:23:03 +0900

    [Gold II] Title: 수열의 구간 평균, Time: 468 ms, Memory: 55048 KB -BaekjoonHub

--- 백준/Gold/19566. 수열의 구간 평균/수열의 구간 평균.java
+++ 백준/Gold/19566. 수열의 구간 평균/수열의 구간 평균.java
@@ -6,33 +6,32 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		long K = Long.parseLong(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
 		Map<Long, Long> map = new HashMap<>();
-		long sum = 0;
 		st = new StringTokenizer(br.readLine());
+		long sum = 0;
 		long result = 0;
 		for (long i = 1; i <= N; i++) {
-			long input = Long.parseLong(st.nextToken());
+			int input = Integer.parseInt(st.nextToken());
 			sum += input;
 
-			long temp = sum - i * K;
+			long key = sum - (i * K);
+			Long value = map.get(key);
 
-			Long value = map.get(temp);
-
-			if (value == null) {
-				map.put(temp, 1L);
-			} else {
+			if (value != null) {
 				result += value;
-				map.put(temp, value + 1L);
+				map.put(key, value + 1);
+			} else {
+				map.put(key, 1L);
 			}
 		}
-		Long value = map.get(0L);
 
-		if (value != null) {
-			result += value;
+		if (map.containsKey(0L)) {
+			result += map.get(0L);
 		}
 
 		System.out.println(result);
+
 	}
 }

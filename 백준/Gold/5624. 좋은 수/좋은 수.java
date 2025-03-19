86b7c3cc10cbf173421bd8e2aaa2900bdb2f400e
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-19 23:36:28 +0900

    [Gold III] Title: 좋은 수, Time: 536 ms, Memory: 190460 KB -BaekjoonHub

--- 백준/Gold/5624. 좋은 수/좋은 수.java
+++ 백준/Gold/5624. 좋은 수/좋은 수.java
@@ -1,17 +1,23 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] data = new int[N];
+		
+		Set<Integer> set = new HashSet<>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
 		}
-		Set<Integer> set = new HashSet<>();
+
 		set.add(data[0] + data[0]);
+		
+		
 
 		int result = 0;
 		for (int i = 1; i < N; i++) {
@@ -26,7 +32,6 @@ public class Main {
 				set.add(data[i] + data[j]);
 			}
 		}
-
 		System.out.println(result);
 	}
 }

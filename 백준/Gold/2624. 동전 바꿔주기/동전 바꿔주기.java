608b244176ad54a38467619612f49019b5a55618
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-03-20 17:21:45 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 192 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
@@ -1,46 +1,29 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int K;
-	public static int[] coin;
-	public static int[] count;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int T = Integer.parseInt(br.readLine());
-		K = Integer.parseInt(br.readLine());
-		coin = new int[K+1];
-		count = new int[K+1];
-		for (int i = 1; i <= K; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int cost = Integer.parseInt(st.nextToken());
-			int cnt = Integer.parseInt(st.nextToken());
-			coin[i] = cost;
-			count[i] = cnt;
-
-		}
-
 		int[] dp = new int[T + 1];
-
 		dp[0] = 1;
-		for (int i = 1; i <= K; i++) {
-			int cost = coin[i];
-			int cnt = count[i];
+
+		int K = Integer.parseInt(br.readLine());
+		for (int i = 0; i < K; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int n = Integer.parseInt(st.nextToken());
 			for (int t = T; t >= 0; t--) {
-				for (int j = 1; j <= cnt; j++) {
-					if (t - cost * j >= 0) {
-						dp[t] += dp[t - cost * j];
+				for (int j = 1; j <= n; j++) {
+					if (t - p * j >= 0) {
+						dp[t] += dp[t - p * j];
 					}
 				}
 			}
 		}
 
-
 		System.out.println(dp[T]);
-
 	}
-
-
 }

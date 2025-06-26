511ca62470bd96b3a4ef6a020ee735d3c93457df
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-06-26 12:26:28 +0900

    [Gold IV] Title: 좋은수열, Time: 132 ms, Memory: 16452 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -1,18 +1,19 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
 	public static List<Integer> list = List.of(1, 2, 3);
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		find(0,"");
+		find(N, 0, "");
 	}
 
-	public static boolean find(int length, String s) {
+	public static boolean find(int N, int length, String s) {
 		if (length == N) {
 			System.out.println(s);
 			return true;
@@ -20,7 +21,7 @@ public class Main {
 
 		for (int i : list) {
 			if (check(s + i)) {
-				if (find(length + 1, s + i)) {
+				if (find(N, length + 1, s + i)) {
 					return true;
 				}
 			}
@@ -31,12 +32,14 @@ public class Main {
 
 	public static boolean check(String s) {
 		int length = s.length();
-		int range= length/2;
+		int range = length/2;
+
 		for (int i = 1; i <= range; i++) {
 			if (s.substring(length - i * 2, length - i).equals(s.substring(length - i))) {
 				return false;
 			}
 		}
+
 
 		return true;
 	}

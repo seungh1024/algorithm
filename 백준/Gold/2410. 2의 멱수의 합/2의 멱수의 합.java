d516530fbd28c4220e7bb8338cdf121663ebac52
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-06-29 15:45:33 +0900

    [Gold V] Title: 2의 멱수의 합, Time: 124 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.java
+++ 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.java
@@ -4,36 +4,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int[] count;
-	public static List<Integer> list;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		count = new int[N+1];
+		long mod = 1000000000;
 
-		list = new ArrayList<>();
-		int idx = 1;
-		while (idx <= N) {
-		list.add(idx);
-			idx *= 2;
-		}
-
-		int[] dp = new int[N + 1];
+		int[] dp = new int[1000001];
+		dp[0] = 1;
 		dp[1] = 1;
-
-		int mod = 1000000000;
 		for (int i = 2; i <= N; i++) {
 			if (i % 2 == 0) {
-				dp[i] = dp[i-1] + dp[i/2];
+				dp[i] += dp[i / 2] + dp[i - 1];
 			} else {
-				dp[i] = dp[i-1];
+				dp[i] += dp[i - 1];
 			}
 			dp[i] %= mod;
 		}
-
 		System.out.println(dp[N]);
-
 	}
-
 }
+
+// 1 1 1 1 1 1
+// 1 1 1 1 2
+// 1 1 2 2
+// 1 1 4
+// 2 4
+// 2 2 2
+//
+//
+// 2.
+// 1 1
+// 2
+//
+// 3.
+// 1 1 1
+// 1 2
+//
+// 4.
+// 1 1 1 1
+// 1 1 2
+// 2 2
+// 4

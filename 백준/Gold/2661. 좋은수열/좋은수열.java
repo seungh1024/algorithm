0d92d7919823b1d6b48afb517b7691731c3f7923
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-05 16:35:55 +0900

    [Gold IV] Title: 좋은수열, Time: 132 ms, Memory: 16368 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -4,42 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static List<Integer> list = List.of(1, 2, 3);
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-
-		find(N, 0, "");
+		find(0, N, "");
 	}
 
-	public static boolean find(int N, int length, String s) {
-		if (length == N) {
+	public static boolean find(int idx, int N, String s) {
+		// System.out.println("idx = "+idx +", s = "+s);
+		if (idx == N) {
 			System.out.println(s);
 			return true;
 		}
 
-		for (int i : list) {
+		for (int i = 1; i <= 3; i++) {
+			// System.out.println("s = "+s +", i = "+i);
 			if (check(s + i)) {
-				if (find(N, length + 1, s + i)) {
+				if (find(idx + 1, N, s + i)) {
 					return true;
 				}
 			}
+			// System.out.println("after s =" + s +", i = "+i);
 		}
+
 
 		return false;
 	}
 
-	public static boolean check(String s) {
+	private static boolean check(String s) {
+		// System.out.println("s = "+s);
 		int length = s.length();
-		int range = length/2;
+		int range = length / 2;
 
 		for (int i = 1; i <= range; i++) {
-			if (s.substring(length - i * 2, length - i).equals(s.substring(length - i))) {
+			String s1 = s.substring(length - i);
+			String s2 = s.substring(length - i * 2, length - i);
+			if (s1.equals(s2)) {
+				// System.out.println("false s = "+s);
 				return false;
 			}
 		}
 
+		// System.out.println("return true");
 
 		return true;
 	}

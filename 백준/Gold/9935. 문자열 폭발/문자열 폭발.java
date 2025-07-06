dd203071fc2c3d753da8da8a6f44d0ca27a1c67b
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-06 19:11:06 +0900

    [Gold IV] Title: 문자열 폭발, Time: 468 ms, Memory: 82680 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -7,65 +7,55 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		String bomb = br.readLine();
-
-		ArrayDeque<Character> q = new ArrayDeque<>();
-		char[] data = s.toCharArray();
-		for (char c : data) {
+		Deque<Character> q = new ArrayDeque<>();
+		char[] input = br.readLine().toCharArray();
+		for (char c : input) {
 			q.offer(c);
 		}
-		// System.out.println(q);
 
-		Map<Character, Integer> map = new HashMap<>();
-		int number = 1;
-		char[] b = bomb.toCharArray();
-		for (char c : b) {
-			if (!map.containsKey(c)) {
-				map.put(c, number++);
-			}
-		}
+		char[] data = br.readLine().toCharArray();
 
-		ArrayDeque<Character> dq = new ArrayDeque<>();
-
-		int maxSize = b.length;
-		Integer idx = 0;
-
+		Deque<Character> q2 = new ArrayDeque<>();
+		int idx = 0;
 		while (!q.isEmpty()) {
-			Character now = q.poll();
-			dq.offer(now);
+			char now = q.poll();
+			if (now == data[idx]) {
+				idx++;
+			} else {
+				idx = 0;
+				if (now == data[idx]) {
+					idx++;
+				}
+			}
+			q2.offer(now);
 
-			// System.out.println("q = " + q);
-			// System.out.println("dq = "+dq);
-
-			if (now == b[maxSize - 1] && dq.size() >= maxSize) {
-				Stack<Character> stack = new Stack<>();
-				boolean check = false;
-				for (int i = maxSize - 1; i >= 0; i--) {
-					Character c = dq.pollLast();
-					stack.push(c);
-					if (c != b[i]) {
-						check = true;
+			// System.out.println("q2 = "+q2);
+			// System.out.println("q = "+q);
+			// System.out.println("idx = "+idx);
+			if (idx == data.length) {
+				for (int i = 0; i < idx; i++) {
+					q2.pollLast();
+				}
+				while (!q2.isEmpty()) {
+					char last = q2.pollLast();
+					q.offerFirst(last);
+					if (last == data[0]) {
 						break;
 					}
 				}
 
-				if (check) {
-					while (!stack.isEmpty()) {
-						dq.offer(stack.pop());
-					}
-				}
+				idx = 0;
 			}
 		}
 
-		if (dq.isEmpty()) {
-			System.out.println("FRULA");
-		} else {
-			StringBuilder sb = new StringBuilder();
-			while (!dq.isEmpty()) {
-				sb.append(dq.poll());
-			}
-			System.out.println(sb);
+		StringBuilder sb = new StringBuilder();
+		while (!q2.isEmpty()) {
+			sb.append(q2.poll());
 		}
+		if (sb.isEmpty()) {
+			sb.append("FRULA");
+		}
+		System.out.println(sb);
+
 	}
 }

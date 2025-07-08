39ae79faa69657da65fa177ddfd49c925db0273a
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-09 00:37:47 +0900

    [Gold I] Title: 곰곰이와 자판기, Time: 736 ms, Memory: 96664 KB -BaekjoonHub

--- 백준/Gold/25200. 곰곰이와 자판기/곰곰이와 자판기.java
+++ 백준/Gold/25200. 곰곰이와 자판기/곰곰이와 자판기.java
@@ -1,47 +1,40 @@
 
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static int N, M;
-	public static int[] parent;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		parent = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			parent[i] = i;
-		}
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		Stack<int[]> stack = new Stack<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			stack.push(new int[] {a, b});
-			// union(a, b);
-			// System.out.println(Arrays.toString(parent));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			stack.push(new int[]{u, v});
 		}
-		while (!stack.isEmpty()) {
+
+		int[] dp = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			dp[i] = i;
+		}
+		while(!stack.isEmpty()) {
 			int[] now = stack.pop();
-			parent[now[0]] = parent[now[1]];
-			// System.out.println(Arrays.toString(parent));
+			int from = now[0];
+			int to = now[1];
+
+			dp[from] = dp[to];
 		}
+		// System.out.println(Arrays.toString(dp));
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= N; i++) {
-			sb.append(parent[i]).append(" ");
+			sb.append(dp[i]).append(" ");
 		}
 		System.out.println(sb);
 	}
-
 }

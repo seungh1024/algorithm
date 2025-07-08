c0df1783b714164f0184b2b759cd1ef4dc5aa39b
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-09 00:56:17 +0900

    [Gold III] Title: 무엇을 아느냐가 아니라 누구를 아느냐가 문제다, Time: 336 ms, Memory: 41852 KB -BaekjoonHub

--- 백준/Gold/9694. 무엇을 아느냐가 아니라 누구를 아느냐가 문제다/무엇을 아느냐가 아니라 누구를 아느냐가 문제다.java
+++ 백준/Gold/9694. 무엇을 아느냐가 아니라 누구를 아느냐가 문제다/무엇을 아느냐가 아니라 누구를 아느냐가 문제다.java
@@ -7,80 +7,78 @@ public class Main {
 	public static int N, M;
 	public static List<Data>[] list;
 
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+
 		for (int t = 1; t <= T; t++) {
-			sb.append("Case #").append(t).append(": ");
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
+
 			list = new ArrayList[M];
 			for (int i = 0; i < M; i++) {
 				list[i] = new ArrayList<>();
 			}
-
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				int cost = Integer.parseInt(st.nextToken());
 
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int z = Integer.parseInt(st.nextToken());
-				list[x].add(new Data(y, z));
-				list[y].add(new Data(x, z));
+				list[from].add(new Data(to, cost));
+				list[to].add(new Data(from, cost));
 			}
 
-
-			sb.append(find()).append("\n");
-
+			String result = find();
+			sb.append("Case #").append(t).append(": ").append(result).append("\n");
 		}
+
 		System.out.println(sb);
 	}
 
 	public static String find() {
-		PriorityQueue<Data> pq = new PriorityQueue<>(
-			Comparator.comparingInt((Data o) -> o.z).thenComparingInt(o -> o.v));
-		pq.offer(new Data(0, 0));
+		PriorityQueue<Data> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
 		int[] distance = new int[M];
 		Arrays.fill(distance, Integer.MAX_VALUE);
 		distance[0] = 0;
+		pq.offer(new Data(0, 0));
 		boolean[] visited = new boolean[M];
 		int[] last = new int[M];
 
-		while (!pq.isEmpty()) {
+		while(!pq.isEmpty()) {
 			Data now = pq.poll();
 
-			if(visited[now.f]) continue;
-			visited[now.f] = true;
-
-			if (now.f == M - 1) {
+			if (now.to == M - 1) {
 				break;
 			}
+			if(visited[now.to]) continue;
+			visited[now.to] = true;
 
-			for (Data next : list[now.f]) {
-				if (!visited[next.f] && distance[next.f] > distance[now.f] + next.z) {
-					distance[next.f] = distance[now.f] + next.z;
-					last[next.f] = now.f;
-					pq.offer(new Data(next.f, distance[next.f], next.z));
+			for(Data next : list[now.to]) {
+				if (!visited[next.to] && distance[next.to] > distance[now.to] + next.cost) {
+					distance[next.to] = distance[now.to] + next.cost;
+					last[next.to] = now.to;
+					pq.offer(new Data(next.to, distance[next.to]));
 				}
 			}
 		}
 
-		// System.out.println(Arrays.toString(distance));
-
-		if (distance[M - 1] == Integer.MAX_VALUE) {
+		if(distance[M-1] == Integer.MAX_VALUE) {
 			return "-1";
 		}
 
+		// System.out.println(Arrays.toString(last));
+		StringBuilder sb = new StringBuilder();
 		Stack<Integer> stack = new Stack<>();
-		int start = M-1;
-		while (start != 0) {
-			stack.push(start);
-			start = last[start];
+		int idx = M-1;
+		while (idx != 0) {
+			stack.push(idx);
+			idx = last[idx];
 		}
 		stack.push(0);
-		StringBuilder sb = new StringBuilder();
 		while (!stack.isEmpty()) {
 			sb.append(stack.pop()).append(" ");
 		}
@@ -89,36 +87,12 @@ public class Main {
 	}
 
 	public static class Data{
-		int f;
-		int z;
-		int v;
+		int to;
+		int cost;
 
-		public Data(int f, int z) {
-			this.f = f;
-			this.z = z;
-		}
-
-		public Data(int f, int z, int v) {
-			this.f = f;
-			this.z = z;
-			this.v = v;
-		}
-
-		@Override
-		public String toString() {
-			return "Data{" +
-				"f=" + f +
-				", z=" + z +
-				", v=" + v +
-				'}';
+		public Data(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
 		}
 	}
 }
-// 7 5
-// 0 1 2
-// 1 3 4
-// 0 2 1
-// 0 4 3
-// 3 2 3
-// 3 4 4
-// 2 4 2

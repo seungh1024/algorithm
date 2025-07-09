75aa0724d1bb4ed6625ed52d3a7016ea7cf3fe68
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-09 15:07:39 +0900

    [Gold III] Title: 마트료시카, Time: 528 ms, Memory: 38820 KB -BaekjoonHub

--- 백준/Gold/23845. 마트료시카/마트료시카.java
+++ 백준/Gold/23845. 마트료시카/마트료시카.java
@@ -7,25 +7,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] data = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			data[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(data);
-
-		int[] dp = new int[100001];
 		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] dp = new int[100001];
 		for (int i = 0; i < N; i++) {
-			pq.offer(data[i]);
-			dp[data[i]]++;
+			int num = Integer.parseInt(st.nextToken());
+			pq.offer(num);
+			dp[num]++;
 		}
-
 
 		long sum = 0;
 		while (!pq.isEmpty()) {
 			int now = pq.poll();
-			if(dp[now] == 0) continue;
+			if(dp[now] ==0) continue;
 
 			long cnt = 0;
 			for (int i = now; i > 0; i--) {
@@ -35,8 +29,9 @@ public class Main {
 				cnt++;
 				dp[i]--;
 			}
-			sum += now*cnt;
+			sum += cnt*now;
 		}
+
 		System.out.println(sum);
 
 	}

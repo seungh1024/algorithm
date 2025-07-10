5326215f8389147abb00881c3cf01049ef285f68
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-11 00:49:14 +0900

    [Gold III] Title: 아침 산책, Time: 2424 ms, Memory: 354420 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.java
+++ 백준/Gold/21606. 아침 산책/아침 산책.java
@@ -8,7 +8,6 @@ public class Main {
 	public static boolean[] inOut;
 	public static List<Integer>[] list;
 	public static boolean[] visited;
-	public static long[] count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,64 +24,67 @@ public class Main {
 			list[i] = new ArrayList<>();
 		}
 
+		long result = 0;
 		for (int i = 1; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			list[from].add(to);
 			list[to].add(from);
-		}
 
-		visited = new boolean[N + 1];
-		count = new long[N + 1];
-		long result = 0;
-		for (int i = 1; i <= N; i++) {
-			if(visited[i]) continue;
-			if (inOut[i]) {
-				find(i);
-				// System.out.println("i = "+i);
-				// System.out.println(Arrays.toString(count));
-				visited[i] = true;
+			if (inOut[from] && inOut[to]) {
+				result+=2;
 			}
 		}
 
+		visited = new boolean[N + 1];
+
 		for (int i = 1; i <= N; i++) {
-			result += count[i];
+			if(visited[i]) continue;
+			if (!inOut[i]) {
+				visited[i] = true;
+				long cnt = find(i);
+				// System.out.println("i = " + i + ", cnt = " + cnt);
+				result += (cnt * (cnt - 1));
+			}
 		}
+
+
 
 
 		System.out.println(result);
 	}
 
-	public static void find(int start) {
+
+	public static long find(int start) {
 		Queue<Integer> q = new ArrayDeque<>();
 		q.offer(start);
 
 		long cnt = 0;
-		List<Integer> last = new ArrayList<>();
+		boolean[] check = new boolean[N + 1];
 		while (!q.isEmpty()) {
 			int now = q.poll();
 
-			if (inOut[now] && start != now) {
-				count[start]++;
-				count[now]++;
+			if (inOut[now]) {
+				cnt++;
 				continue;
+			} else {
+				visited[now] = true;
 			}
 
 			for (int next : list[now]) {
-				if (!visited[next]) {
-					last.add(next);
-					visited[next] = true;
+				if (!check[next]) {
+					check[next] = true;
 					q.offer(next);
 				}
 			}
 		}
 
-		for (int i : last) {
-			if (i!=start) {
-				visited[i] = false;
-			}
+		if (cnt < 2) {
+			cnt = 0;
 		}
+
+		return cnt;
 	}
 
 }

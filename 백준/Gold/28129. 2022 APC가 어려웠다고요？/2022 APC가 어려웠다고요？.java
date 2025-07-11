25244007b0fa3148a162c9a4732fd76e9a586d59
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-11 16:44:52 +0900

    [Gold II] Title: 2022 APC가 어려웠다고요?, Time: 452 ms, Memory: 92924 KB -BaekjoonHub

--- 백준/Gold/28129. 2022 APC가 어려웠다고요？/2022 APC가 어려웠다고요？.java
+++ 백준/Gold/28129. 2022 APC가 어려웠다고요？/2022 APC가 어려웠다고요？.java
@@ -12,53 +12,42 @@ public class Main {
 
 		long[][] dp = new long[3001][3001];
 		// Arrays.fill(dp[0], 1);
-		// st = new StringTokenizer(br.readLine());
-		// int a = Integer.parseInt(st.nextToken());
-		// int b = Integer.parseInt(st.nextToken());
-		// for (int i = a; i <= b; i++) {
-		// 	dp[1][i] = 1;
-		// }
-		// for (int j = 1; j <= 3000; j++) {
-		// 	dp[1][j] += dp[1][j - 1];
-		// }
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		for (int i = a; i <= b; i++) {
+			dp[1][i] = 1;
+		}
+		for (int j = 1; j <= 3000; j++) {
+			dp[1][j] += dp[1][j - 1];
+		}
 		// System.out.println();
 
 		// System.out.println(Arrays.toString(dp[1]));
 
-		long[][] sum = new long[3001][3001];
+
 		long mod = 1000000007;
-		int a = 0;
-		int b = 0;
-		for (int i = 1; i <= N; i++) {
+		for (int i = 2; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			if (i == 1) {
-				for (int j = start; j <= end; j++) {
-					dp[i][j] = 1;
-				}
-				a = start;
-				b = end;
-			} else {
+			for (int j = start; j <= end; j++) {
+				int max = Math.min(j + K, b);
+				int min = Math.max(a, j - K);
 
-				for (int j = start; j <= end; j++) {
-					int max = Math.min(j + K, b);
-					int min = Math.max(a, j - K);
-
-					dp[i][j] = (sum[i - 1][max] - sum[i - 1][min-1] + mod);
-					dp[i][j] %= mod;
-				}
-				a = start;
-				b = end;
+				dp[i][j] = (dp[i - 1][max] - dp[i - 1][min-1] + mod);
+				dp[i][j] %= mod;
 			}
+			a = start;
+			b = end;
 			for (int j = 1; j <= 3000; j++) {
-				sum[i][j] += sum[i][j - 1] + dp[i][j];
-				sum[i][j] %= mod;
+				dp[i][j] += dp[i][j-1];
+				dp[i][j] %= mod;
 			}
 			// System.out.println(Arrays.toString(dp[i]));
 		}
 
-		System.out.println(sum[N][3000]);
+		System.out.println(dp[N][3000]);
 	}
 }
 

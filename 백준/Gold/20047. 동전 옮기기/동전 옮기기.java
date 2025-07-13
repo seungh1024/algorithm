a24473ecb6556b10bd69d0562b17b0a716dd3c11
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-13 13:50:34 +0900

    [Gold II] Title: 동전 옮기기, Time: 108 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Gold/20047. 동전 옮기기/동전 옮기기.java
+++ 백준/Gold/20047. 동전 옮기기/동전 옮기기.java
@@ -4,64 +4,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
-	public static boolean[][] dp;
-	public static char[] data, S, T;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-		char[] input = br.readLine().toCharArray();
-		S = new char[N];
-		T = br.readLine().toCharArray();
-		data = new char[2];
+		int N = Integer.parseInt(br.readLine());
+		boolean[][] dp = new boolean[N][3];
+		char[] S = br.readLine().toCharArray();
+		char[] T = br.readLine().toCharArray();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
-		data[0] = input[a];
-		data[1] = input[b];
-
+		char[] data = {S[a], S[b]};
+		char[] temp = new char[N];
 		int idx = 0;
 		for (int i = 0; i < N; i++) {
-			if(a == i || b == i)continue;
-			S[idx++] = input[i];
+			if(i == a || i == b)
+				continue;
+			temp[idx++] = S[i];
+		}
+		for (int i = 0; i < N; i++) {
+			S[i] = temp[i];
+		}
+		S[N-1] = 'a';
+		S[N-2] = 'a';
+
+		if (S[0] == T[0]) {
+			dp[0][0] = true;
+		}
+		if (T[0] == data[0]) {
+			dp[0][1] = true;
 		}
 
-		dp = new boolean[3][N];
 
-		if (find(0, 0)) {
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = dp[i - 1][0] && (S[i] == T[i]);
+			dp[i][1] = dp[i - 1][0] && (data[0] == T[i]) || dp[i - 1][1] && (S[i - 1] == T[i]);
+			dp[i][2] = dp[i - 1][1] && (data[1] == T[i]) || dp[i - 1][2] && (S[i - 2] == T[i]);
+		}
 
+		if (dp[N-1][2]) {
 			System.out.println("YES");
 		} else {
 			System.out.println("NO");
 		}
-	}
-
-	public static boolean find(int status, int idx) {
-		if (status+idx == N-1) {
-			return true;
-		}
-
-		if (idx == N - 2) {
-			return false;
-		}
-
-		if (S[idx] == T[idx + status]) {
-			if ((find(status, idx + 1))) {
-				return true;
-			}
-		}
 
 
-		if(status == 2) return false;
-
-		if (data[status] == T[idx + status]) {
-			if (find(status + 1, idx)) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 }

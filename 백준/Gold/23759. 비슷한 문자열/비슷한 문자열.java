13947a8f63e5aabf9b65d79a0c113cf62a10dc24
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-17 17:01:24 +0900

    [Gold II] Title: 비슷한 문자열, Time: 372 ms, Memory: 88148 KB -BaekjoonHub

---
+++ 백준/Gold/23759. 비슷한 문자열/비슷한 문자열.java
@@ -0,0 +1,36 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		char[][] data = new char[N+1][K];
+		int[][] dp = new int[27][K];
+		for (int i = 0; i < N; i++) {
+			int max = 0;
+			data[i] =br.readLine().toCharArray();
+			for (int j = 0; j < K; j++) {
+				dp[data[i][j]-'a'][j]++;
+				max = Math.max(max, dp[data[i][j]-'a'][j]);
+			}
+
+			for (int j = 0; j < K; j++) {
+				dp[data[i][j]-'a'][j] = max;
+			}
+		}
+
+		int result = 0;
+		for (int i = 0; i < 27; i++) {
+			for (int j = 0; j < K; j++) {
+				result = Math.max(dp[i][j], result);
+			}
+		}
+
+		System.out.println(N - result);
+	}
+}

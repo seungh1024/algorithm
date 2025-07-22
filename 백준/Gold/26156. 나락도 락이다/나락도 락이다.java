538510ee15fd6db146ea51120cb57c72fe57af18
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-22 20:54:28 +0900

    [Gold III] Title: 나락도 락이다, Time: 300 ms, Memory: 32272 KB -BaekjoonHub

--- 백준/Gold/26156. 나락도 락이다/나락도 락이다.java
+++ 백준/Gold/26156. 나락도 락이다/나락도 락이다.java
@@ -4,17 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int N;
+	public static char[] data;
+	public static char[] ch = {'R', 'O', 'C', 'K'};
+	public static int[] count;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		char[] data = new char[N+1];
-		char[] input = br.readLine().toCharArray();
-		for (int i = 1; i <= N; i++) {
-			data[i] = input[i - 1];
-		}
-		long[] dp = new long[4];
-
+		N = Integer.parseInt(br.readLine());
 		long mod = 1000000007;
+
+		long[] dp = new long[4];
+		char[] input = br.readLine().toCharArray();
+		data = new char[N + 1];
+		int idx = 1;
+		for (char c : input) {
+			data[idx++] = c;
+		}
+
 		long[] value = new long[N + 1];
 		value[1] = 1;
 		for (int i = 2; i <= N; i++) {
@@ -26,17 +33,14 @@ public class Main {
 				dp[3]++;
 			} else if (data[i] == 'C') {
 				dp[2] = (dp[2] + dp[3]) % mod;
-			}else if (data[i] == 'O') {
+			} else if (data[i] == 'O') {
 				dp[1] = (dp[1] + dp[2]) % mod;
 			} else if (data[i] == 'R') {
-
-				dp[0] = (dp[0] + value[i]  * dp[1] % mod) % mod;
+				dp[0] = (dp[0] + value[i] * dp[1] % mod) % mod;
 			}
-
 		}
-
 		System.out.println(dp[0]);
-
+		
 	}
 
 }

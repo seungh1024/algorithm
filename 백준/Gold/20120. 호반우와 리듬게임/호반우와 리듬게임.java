3a3da456dabe242f10dbce0a7c81908533cda013
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-07-22 14:06:32 +0900

    [Gold III] Title: 호반우와 리듬게임, Time: 124 ms, Memory: 22672 KB -BaekjoonHub

--- 백준/Gold/20120. 호반우와 리듬게임/호반우와 리듬게임.java
+++ 백준/Gold/20120. 호반우와 리듬게임/호반우와 리듬게임.java
@@ -8,9 +8,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		long[] dp = new long[1001];
-		dp[0] = 0;
-		dp[1] = Long.parseLong(st.nextToken());
+		long[][] dp = new long[1001][1001];
+
+		dp[1][1] = Long.parseLong(st.nextToken());
 
 		long k1 = 0;
 		long k2 = 0;
@@ -20,19 +20,20 @@ public class Main {
 			int t = Integer.parseInt(st.nextToken());
 			tmp = Long.MIN_VALUE;
 			for (int j = i; j >= 1; j--) {
-				tmp = Math.max(tmp, dp[j]);
-				dp[j + 1] = dp[j] + (j + 1L) * t;
+				tmp = Math.max(tmp, dp[i][j]);
+				dp[i+1][j + 1] = dp[i][j] + (j + 1L) * t;
 			}
 
-			dp[0] = Math.max(k1, k2);
-			dp[1] = dp[0] + t;
+			dp[i+1][0] = Math.max(k1, k2);
+			dp[i+1][1] = dp[i+1][0] + t;
 			k2 = k1;
 			k1 = tmp;
 		}
 
 		tmp = Math.max(k1, k2);
 		for (int i = 1; i <= n; i++) {
-			tmp = Math.max(tmp, dp[i]);
+			tmp = Math.max(tmp, dp[n][i]);
+			// System.out.println(Arrays.toString(dp[i]));
 		}
 
 		System.out.println(Math.max(tmp, 0));

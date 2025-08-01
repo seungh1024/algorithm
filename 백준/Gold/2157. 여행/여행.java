b1f95dbe354e3b51cb19b2397ee8a172517ffa76
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-01 12:30:29 +0900

    [Gold IV] Title: 여행, Time: 364 ms, Memory: 43120 KB -BaekjoonHub

--- 백준/Gold/2157. 여행/여행.java
+++ 백준/Gold/2157. 여행/여행.java
@@ -1,12 +1,12 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N, M, K;
+	public static int N,M, K;
 	public static List<Data>[] list;
 
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
@@ -18,66 +18,72 @@ public class Main {
 			list[i] = new ArrayList<>();
 		}
 
+		int[][] max = new int[N+1][N+1];
+
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 			int cost = Integer.parseInt(st.nextToken());
-
 			if (from < to) {
-				list[from].add(new Data(to, cost));
+				max[from][to] = Math.max(max[from][to], cost);
 			}
 		}
 
-		find();
-	}
+		int[][] dp = new int[N+1][M+1];
+		for (int i = 1; i <= N; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		dp[1][1] = 0;
 
-	public static void find() {
-		Queue<Data> q = new ArrayDeque<>();
-		q.offer(new Data(1, 0));
-		int m = 1;
-		int[][] distance = new int[N+1][M+1];
-		distance[1][0] = -1;
-
-		int result = 0;
-		while (!q.isEmpty()) {
-			int size = q.size();
-
-			for (int s = 0; s < size; s++) {
-				Data now = q.poll();
-
-				if(distance[now.to][m] > now.cost) continue;
-				// distance[now.to] = now.cost;
-
-				// result = Math.max(result,now.cost);
-				for (Data next : list[now.to]) {
-					if (distance[next.to][m+1] < distance[now.to][m] + next.cost) {
-						distance[next.to][m+1] = distance[now.to][m] + next.cost;
-						q.offer(new Data(next.to, distance[next.to][m+1]));
+		for (int i = 1; i <= N; i++) {
+			for (int j = i + 1; j <= N; j++) {
+				if (max[i][j] > 0) {
+					for (int m = 1; m < M; m++) {
+						if (dp[i][m] >=0) {
+							dp[j][m + 1] = Math.max(dp[j][m + 1], dp[i][m] + max[i][j]);
+							// System.out.println("i = "+ i +", j = "+j + ", m = "+m +", dp = "+dp[j][m+1]);
+							// System.out.println(dp[2][2]);
+						}
 					}
 				}
 			}
-			m++;
-			if (m == M) {
-				break;
-			}
 		}
 
-		// System.out.println(Arrays.toString(distance));
-
-		for (int i = 0; i <= M; i++) {
-			result = Math.max(result, distance[N][i]);
+		int result = 0;
+		for (int i = 2; i <= M; i++) {
+			result = Math.max(result, dp[N][i]);
 		}
 		System.out.println(result);
 	}
 
+
 	public static class Data{
 		int to;
 		int cost;
+		int m;
 
-		public Data(int to, int cost) {
+		public Data(int to, int cost, int m) {
 			this.to = to;
 			this.cost = cost;
+			this.m = m;
+		}
+
+		@Override
+		public String toString() {
+			return "Data{" +
+				"to=" + to +
+				", cost=" + cost +
+				", m=" + m +
+				'}';
 		}
 	}
+
 }
+
+// 3 3 5
+// 1 3 1
+// 1 2 1
+// 2 3 2
+// 3 1 4
+// 3 1 100

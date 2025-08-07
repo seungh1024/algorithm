816bef22ecdcf102df668bbfd1d61f8ced42a5d9
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-07 20:47:14 +0900

    [Gold III] Title: 군탈체포조, Time: 2128 ms, Memory: 297636 KB -BaekjoonHub

--- 백준/Gold/23354. 군탈체포조/군탈체포조.java
+++ 백준/Gold/23354. 군탈체포조/군탈체포조.java
@@ -4,169 +4,133 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int[] arr;
-	public static List<Data>[] list;
 	public static int N;
 	public static int[][] data;
+	public static List<Data> list;
+	public static boolean[] visited;
+	public static int[] index;
 	public static int[] dx = {0, 0, 1, -1};
 	public static int[] dy = {1, -1, 0, 0};
-	public static int result;
+	public static int result = Integer.MAX_VALUE;
+	public static int[][] dis;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N][N];
-		list = new ArrayList[N+1];
-		for (int i = 0; i <= N; i++) {
-			list[i] = new ArrayList<>();
-		}
-
-		int idx = 0;
-		int x = 0;
-		int y = 0;
-		List<int[]> dataList = new ArrayList<>();
+		list = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				data[i][j] = Integer.parseInt(st.nextToken());
-				if (data[i][j] == 0) {
-					dataList.add(new int[] {i, j,idx++});
-				} else if (data[i][j] == -1) {
-					x=i;
-					y=j;
+				if (data[i][j] == -1) {
 					data[i][j] = 0;
+					list.add(0,new Data(i, j));
+				} else if (data[i][j] == 0) {
+					list.add(new Data(i, j));
 				}
 			}
-
-		}
-		if (idx == 0) {
-			System.out.println(0);
-			return;
 		}
 
-		dataList.add(new int[] {x, y, idx});
-		for (int[] i : dataList) {
-			find(i[0], i[1], dataList, i[2]);
+		visited = new boolean[list.size()];
+		index = new int[list.size()];
+		dis = new int[list.size()][list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = i + 1; j < list.size(); j++) {
+				int v = getValue(list.get(i), list.get(j));
+				// System.out.println(list.get(i));
+				// System.out.println(list.get(j));
+				// System.out.println("i = " + i + ", j = " + j + ", v = " + v);
+				dis[i][j] = v;
+				dis[j][i] = v;
+			}
+			// System.out.println(Arrays.toString(dis[i]));
 		}
 
-		// System.out.println(list[idx]);
-
-		arr = new int[idx];
-		result = Integer.MAX_VALUE;
-		func(0, idx, new boolean[idx]);
+		find(1);
 		System.out.println(result);
 	}
 
-	public static void func(int idx, int N, boolean[] visited) {
-		if (idx == N) {
-			// System.out.println(Arrays.toString(arr));
+	public static int getValue(Data start, Data end) {
+		PriorityQueue<Data> q = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
+		q.offer(start);
+		int[][] check = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(check[i], Integer.MAX_VALUE);
+		}
+		check[start.x][start.y] = 0;
+
+		while (!q.isEmpty()) {
+			Data now = q.poll();
+
+			if (now.x == end.x && now.y == end.y) {
+				break;
+			}
+
+			for (int d = 0; d < 4; d++) {
+				int nx = now.x + dx[d];
+				int ny = now.y + dy[d];
+				if (nx >= 0 && nx < N && ny >= 0 && ny < N && check[nx][ny] > now.cost + data[nx][ny]) {
+					check[nx][ny] = now.cost + data[nx][ny];
+					q.offer(new Data(nx, ny, check[nx][ny]));
+				}
+			}
+
+		}
+
+		return check[end.x][end.y];
+	}
+
+	public static void find(int idx) {
+		if (idx == list.size()) {
 			int sum = 0;
-			for (int i = 1; i < N; i++) {
-				int from = arr[i-1];
-				int to = arr[i];
-				for (Data d : list[from]) {
-					if (d.to == to) {
-						sum += d.cost;
-						break;
-					}
-				}
-				// System.out.println("from = "+from +", to = "+to + ", sum = "+sum);
+			for (int i = 1; i < list.size(); i++) {
+				int from = index[i];
+				int to = index[i-1];
+				sum += dis[from][to];
 			}
-
-			// System.out.println("first sum = "+sum);
-			for (Data d : list[N]) {
-				if (d.to == arr[0]) {
-					sum += d.cost;
-					break;
-				}
-			}
-			for (Data d : list[arr[idx-1]]) {
-				if (d.to == N) {
-					sum+=d.cost;
-					break;
-				}
-			}
-
-			// System.out.println("sum  = "+sum);
+			int from = index[list.size()-1];
+			int to = 0;
+			sum += dis[from][to];
+			// System.out.println(Arrays.toString(index));
+			// System.out.println(sum);
 			result = Math.min(result, sum);
 
 			return;
 		}
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i < list.size(); i++) {
 			if(visited[i]) continue;
 			visited[i] = true;
-			arr[idx] = i;
-			func(idx+1,N,visited);
+			index[idx] = i;
+			find(idx + 1);
 			visited[i] = false;
 		}
 	}
 
-	public static void find(int x, int y, List<int[]> dataList, int idx) {
-		PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o->o[2]));
-		q.offer(new int[] {x, y,0});
-		int[][] visited = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(visited[i], Integer.MAX_VALUE);
-		}
-		visited[x][y] = 0;
-
-		// System.out.println(" x = "+x + ", y ="+y + ", dix = "+idx);
-		while (!q.isEmpty()) {
-			int[] now = q.poll();
-
-			if(visited[now[0]][now[1]] < now[2]) continue;
-			// System.out.println("now = " + Arrays.toString(now) + ", now visit = " + visited[now[0]][now[1]]);
-			for (int d = 0; d < 4; d++) {
-				int nx = now[0]+dx[d];
-				int ny = now[1] + dy[d];
-
-				if (nx >= 0 && nx < N && ny >= 0 && ny < N && visited[nx][ny] > visited[now[0]][now[1]] + data[nx][ny]) {
-					visited[nx][ny] = visited[now[0]][now[1]] + data[nx][ny];
-					// System.out.println("nx = "+nx + ", ny = "+ny + ", next visit = "+visited[nx][ny]);
-					q.offer(new int[] {nx, ny,visited[nx][ny]});
-				}
-			}
-		}
-
-		// for (int i = 0; i < N; i++) {
-		// 	System.out.println(Arrays.toString(visited[i]));
-		// }
-
-
-		for (int[] next : dataList) {
-			int nx=  next[0];
-			int ny = next[1];
-			int nextIdx = next[2];
-
-			if(nextIdx == idx) continue;
-
-			list[idx].add(new Data(nextIdx, visited[nx][ny]));
-			// list[nextIdx].add(new Data(idx, visited[nx][ny]));
-		}
-	}
-
 	public static class Data{
-		int to;
+		int x;
+		int y;
 		int cost;
 
-		public Data(int to, int cost) {
-			this.to = to;
+		public Data(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public Data(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
 			this.cost = cost;
 		}
 
 		@Override
 		public String toString() {
 			return "Data{" +
-				"to=" + to +
+				"x=" + x +
+				", y=" + y +
 				", cost=" + cost +
 				'}';
 		}
 	}
 }
-// 5
-// 1 3 2 1 9
-// 2 1 4 4 3
-// 5 3 -1 1 1
-// 3 7 2 1 1
-// 1 2 3 5 1

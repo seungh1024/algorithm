2715e00b1095874cbe41d1eeb05c5835ed3b001c
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-08 17:54:24 +0900

    [Gold II] Title: 비슷한 문자열, Time: 352 ms, Memory: 66320 KB -BaekjoonHub

--- 백준/Gold/23759. 비슷한 문자열/비슷한 문자열.java
+++ 백준/Gold/23759. 비슷한 문자열/비슷한 문자열.java
@@ -4,26 +4,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		char[][] data = new char[N+1][K];
 		int[][] dp = new int[27][K];
+		
 		for (int i = 0; i < N; i++) {
 			int max = 0;
-			data[i] =br.readLine().toCharArray();
-			for (int j = 0; j < K; j++) {
-				dp[data[i][j]-'a'][j]++;
-				max = Math.max(max, dp[data[i][j]-'a'][j]);
-			}
 
+			char[] data = br.readLine().toCharArray();
 			for (int j = 0; j < K; j++) {
-				dp[data[i][j]-'a'][j] = max;
+				dp[data[j]-'a'][j]++;
+				max = Math.max(max, dp[data[j] - 'a'][j]);
+			}
+			for (int j = 0; j < K; j++) {
+				dp[data[j]-'a'][j] = max;
 			}
 		}
-
+		
 		int result = 0;
 		for (int i = 0; i < 27; i++) {
 			for (int j = 0; j < K; j++) {
@@ -31,6 +32,7 @@ public class Main {
 			}
 		}
 
-		System.out.println(N - result);
+		System.out.println(N-result);
+		
 	}
 }

8fb4f0e6bbec5b89b0997f4ec4d485cc3cc03c94
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-08 16:19:57 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 396 ms, Memory: 33468 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,39 +1,41 @@
 
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] data = new int[N];
 		boolean[] visited = new boolean[1000001];
-		int[] pos = new int[1000001];
+		int[] data = new int[N];
+		int[] indexes = new int[1000001];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			data[i] = Integer.parseInt(st.nextToken());
-			visited[data[i]] = true;
-			pos[data[i]] = i;
+			int idx = Integer.parseInt(st.nextToken());
+			visited[idx] = true;
+			data[i] = idx;
+			indexes[idx] = i;
 		}
 
-		int[] result = new int[N];
+		int[] count = new int[N];
 		for (int i = 0; i < N; i++) {
-			int num = data[i];
-			for (int j = num * 2; j <= 1000000; j += num) {
-				if (visited[j]) {
-					result[i]++;
-					result[pos[j]]--;
+			int v = data[i];
+			for (int j = v * 2; j <= 1000000; j += v) {
+				if(!visited[j]) continue;
+				int idx = indexes[j];
+				int v2 = data[idx];
+				if (v2 % v == 0) {
+					count[i]++;
+					count[idx]--;
 				}
 			}
-			// System.out.println(Arrays.toString(result));
-
 		}
-
+		// System.out.println(Arrays.toString(count));
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			sb.append(result[i]).append(" ");
+			sb.append(count[i]).append(" ");
 		}
 		System.out.println(sb);
 	}

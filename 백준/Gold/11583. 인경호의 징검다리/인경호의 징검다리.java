62d909fa0a80401c2f2b07d6a451465bd1cf558f
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-08 19:24:45 +0900

    [Gold II] Title: 인경호의 징검다리, Time: 1412 ms, Memory: 271904 KB -BaekjoonHub

--- 백준/Gold/11583. 인경호의 징검다리/인경호의 징검다리.java
+++ 백준/Gold/11583. 인경호의 징검다리/인경호의 징검다리.java
@@ -4,60 +4,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N, K;
-	public static int[] data;
-	public static int[] two;
-	public static int[] five;
-	public static int[][] dp;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			data = new int[N];
-			two = new int[N+K];
-			five = new int[N+K];
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			int[] data = new int[N + 1];
+			int[] two = new int[N + 1];
+			int[] five = new int[N + 1];
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
+			for (int i = 1; i <= N; i++) {
 				data[i] = Integer.parseInt(st.nextToken());
-
-				int value = data[i];
-				while (value % 2 == 0) {
-					value/= 2;
-					two[i]++;
+				int a = 0;
+				int num = data[i];
+				while (num % 2 == 0) {
+					a++;
+					num /= 2;
 				}
-				while (value % 5 == 0) {
-					value/=5;
-					five[i]++;
+				two[i] = a;
+				int b = 0;
+				num = data[i];
+				while (num % 5 == 0) {
+					b++;
+					num /= 5;
 				}
+				five[i] = b;
 			}
 
-			dp = new int[N][2];
-			for (int i = 0; i < N; i++) {
-
+			// System.out.println(Arrays.toString(two));
+			// System.out.println(Arrays.toString(five));
+			int[][] dp = new int[N + 1][2];
+			for (int i = 1; i <= N; i++) {
 				Arrays.fill(dp[i], Integer.MAX_VALUE);
 			}
-			dp[0][0] = two[0];
-			dp[0][1] = five[0];
-			for (int i = 1; i < N; i++) {
-				for (int j = 1; j <= K; j++) {
-					if(i-j<0) break;
-					// System.out.println("i = "+i +", j ="+j);
-					dp[i][0] = Math.min(dp[i][0], dp[i-j][0] + two[i]);
-					dp[i][1] = Math.min(dp[i][1], dp[i-j][1] + five[i]);
+			dp[1][0] = two[1];
+			dp[1][1] = five[1];
+
+			for (int i = 1; i <= N; i++) {
+				if(dp[i][0] == Integer.MAX_VALUE || dp[i][1] == Integer.MAX_VALUE)continue;
+				for (int k = 1; k <= K; k++) {
+					if(i+k >N)continue;
+					// int plus = two[i + k] + five[i + k];
+
+					dp[i + k][0] = Math.min(dp[i + k][0], dp[i][0]+two[i+k]);
+					dp[i + k][1] = Math.min(dp[i + k][1], dp[i][1]+five[i+k]);
 				}
-				// System.out.println(Arrays.toString(dp[i]));
 			}
-			// System.out.println(Math.min(dp[N - 1][0], dp[N - 1][1]));
-			sb.append(Math.min(dp[N - 1][0], dp[N - 1][1])).append("\n");
-			
+			// for (int i = 1; i <= N; i++) {
+			// 	System.out.println(Arrays.toString(dp[i]));
+			// }
+
+			sb.append(Math.min(dp[N][0], dp[N][1])).append("\n");
 		}
 		System.out.println(sb);
-
-
 	}
 }

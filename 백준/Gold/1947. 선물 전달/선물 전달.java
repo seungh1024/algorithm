8ee26b5a8d07ef64f7c2e9ab6b945db8e4f49485
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-16 17:20:00 +0900

    [Gold III] Title: 선물 전달, Time: 136 ms, Memory: 22232 KB -BaekjoonHub

--- 백준/Gold/1947. 선물 전달/선물 전달.java
+++ 백준/Gold/1947. 선물 전달/선물 전달.java
@@ -6,17 +6,18 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long mod = 1000_000_000;
-		int N = Integer.parseInt(br.readLine());
+		long N = Long.parseLong(br.readLine());
 		long[] dp = new long[1000001];
-
 		dp[2] = 1;
+		long mod = 1000000000;
 
 		for (int i = 3; i <= N; i++) {
-			long a = ((i-1)*dp[i-2])%mod;
-			long b = ((i-1)*dp[i-1])%mod;
-			dp[i] = (a+b)%mod;
+			dp[i] += dp[i - 2] * (i-1); // i가 서로 주고 받을 때
+			dp[i] %= mod;
+
+			dp[i] += dp[i - 1] * (i - 1); // 일방적으로 주는 경우
+			dp[i] %= mod;
 		}
-		System.out.println(dp[N]);
+		System.out.println(dp[(int)N]);
 	}
 }

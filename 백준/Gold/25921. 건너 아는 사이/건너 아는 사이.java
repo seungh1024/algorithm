2f1d3cc48d73dcea581a35092c903092ac74ccaa
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-16 17:37:11 +0900

    [Gold III] Title: 건너 아는 사이, Time: 128 ms, Memory: 15212 KB -BaekjoonHub

--- 백준/Gold/25921. 건너 아는 사이/건너 아는 사이.java
+++ 백준/Gold/25921. 건너 아는 사이/건너 아는 사이.java
@@ -4,29 +4,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		boolean[] visited = new boolean[N + 1];
-
-		int range = (int)Math.sqrt(N)+1;
+		int N = Integer.parseInt(br.readLine());
 		long sum = 0;
-		for (int i = 2; i < range; i++) {
-			for (int j = i * 2; j <= N; j += i) {
-				if(visited[j]) continue;
-				visited[j] = true;
-				sum += i;
+		boolean[] visited = new boolean[N + 1];
+		int range = (int)Math.sqrt(N);
+		for (int i = 2; i <= range; i++) {
+			for (int j = i+i; j <= N; j += i) {
+				if (!visited[j]) {
+					
+					visited[j] = true;
+					sum +=i;
+				}
 			}
 		}
-
 		for (int i = 2; i <= N; i++) {
-			if (!visited[i]) {
+			if(!visited[i]){
 				sum += i;
 			}
 		}
 		System.out.println(sum);
 	}
-
 }

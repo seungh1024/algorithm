440a0ab84dab9c8d3bbcb178ed8197bd7b0e40ab
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-18 16:36:49 +0900

    [Gold IV] Title: 개근상, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/1563. 개근상/개근상.java
+++ 백준/Gold/1563. 개근상/개근상.java
@@ -7,40 +7,32 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long mod = 1000000;
 
+		// [N][지각횟수][연속된결석횟수]
 		int[][][] dp = new int[N + 1][2][3];
-		dp[1][0][0] = 1;
-		dp[1][1][0] = 1;
-		dp[1][0][1] = 1;
+		dp[0][0][0] = 1;
 
-		for (int i = 2; i <= N; i++) {
-			dp[i][0][0] = dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2];
-			dp[i][0][1] = dp[i - 1][0][0];
-			dp[i][0][2] = dp[i - 1][0][1];
 
-			dp[i][0][0]%=mod;
-			dp[i][0][1]%=mod;
-			dp[i][0][2]%=mod;
+		int mod = 1000000;
+		for (int i = 1; i <= N; i++) {
+			dp[i][0][0] = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2]) % mod;
+			dp[i][1][0] = (dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2] + dp[i-1][1][0]+dp[i-1][1][1]+dp[i-1][1][2]) % mod;
+			dp[i][0][1] = (dp[i - 1][0][0]) % mod;
+			dp[i][0][2] = (dp[i - 1][0][1]) % mod;
+			dp[i][1][1] = (dp[i - 1][1][0]) % mod;
+			dp[i][1][2] = (dp[i - 1][1][1]) % mod;
 
-			dp[i][1][0] = dp[i - 1][0][0] + dp[i - 1][0][1] + dp[i - 1][0][2] + dp[i - 1][1][0] + dp[i - 1][1][1] + dp[i
-				- 1][1][2];
-			dp[i][1][1] = dp[i - 1][1][0];
-			dp[i][1][2] = dp[i - 1][1][1];
-
-			dp[i][1][0] %= mod;
-			dp[i][1][1] %= mod;
-			dp[i][1][2] %= mod;
 		}
 
 		int sum = 0;
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 3; j++) {
-				sum+=dp[N][i][j];
+				sum += (dp[N][i][j]);
 				sum %= mod;
 			}
 		}
 		System.out.println(sum);
+
 
 	}
 }

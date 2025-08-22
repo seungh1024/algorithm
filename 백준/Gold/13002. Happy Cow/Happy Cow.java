2834391bfe95ebc08da9aba1b09f4c38f2ad8ede
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-22 19:45:56 +0900

    [Gold III] Title: Happy Cow, Time: 156 ms, Memory: 31096 KB -BaekjoonHub

--- 백준/Gold/13002. Happy Cow/Happy Cow.java
+++ 백준/Gold/13002. Happy Cow/Happy Cow.java
@@ -5,39 +5,41 @@ import java.util.*;
 
 public class Main {
 	public static int N;
-	public static long[] data;
-	public static long[] arr;
-	public static long[][] dp;
+	public static int[] data;
+	public static int result = 0;
+	public static int[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		data = new long[N+1];
-		arr = new long[N+1];
+		data = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			data[i] = Long.parseLong(st.nextToken());
-			arr[i] = i;
+		for (int i = 0; i < N; i++) {
+			data[i] = Integer.parseInt(st.nextToken());
 		}
 
-		dp = new long[2001][2001];
-		for (int i = 1; i <= N; i++) {
-			Arrays.fill(dp[i], -1);
-		}
+		dp = new int[N][N];
+		result = find(0, N - 1, 1, 0);
 
-		System.out.println(find(1, N, 1));
+		System.out.println(result);
 	}
 
-	public static long find(int left, int right, int day) {
-		if(left > right)
+	public static int find(int left, int right, int day, int sum) {
+		if (left > right) {
 			return 0;
+		}
 
-		if(dp[left][right] != -1) {
+		if (dp[left][right] > 0) {
 			return dp[left][right];
 		}
 
-		return dp[left][right] = Math.max(data[left] * day + find(left + 1, right, day + 1),  data[right] * day + find(left, right - 1, day + 1));
+
+		int max = 0;
+		max = find(left + 1, right, day + 1, sum + data[left] * day) + data[left]*day;
+		max = Math.max(find(left, right - 1, day + 1, sum + data[right] * day)+data[right]*day, max);
+
+		return dp[left][right] = max;
+
+
 	}
-
 }
-

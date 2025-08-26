3f02ec04185665a826fa0ef2911c2edd03a991d3
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-26 21:15:00 +0900

    [Gold V] Title: 조 짜기, Time: 136 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.java
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] data = new int[N+1];
+		int[] data = new int[N + 1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
@@ -15,15 +15,15 @@ public class Main {
 
 		int[] dp = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			int min = 10001;
-			int max = 0;
-			for (int j = i; j > 0; j--) {
-				min = Math.min(min, data[j]);
+			int max = data[i];
+			int min = data[i];
+			for (int j = i + 1; j <= N; j++) {
+
 				max = Math.max(max, data[j]);
-				dp[i] = Math.max(dp[i], dp[j - 1] + max - min);
+				min = Math.min(min, data[j]);
+				dp[j] = Math.max(dp[j], dp[i - 1] + max - min);
 			}
 		}
 		System.out.println(dp[N]);
 	}
-
 }

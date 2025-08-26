9fe898f9d867ce4a6f6ecaeb0f451f7657ebdc83
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-26 17:27:43 +0900

    [Gold II] Title: 펭귄 네비게이터, Time: 712 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Gold/21739. 펭귄 네비게이터/펭귄 네비게이터.java
+++ 백준/Gold/21739. 펭귄 네비게이터/펭귄 네비게이터.java
@@ -7,17 +7,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long mod = 1000000007;
 		long[] dp = new long[10001];
 		dp[0] = 1;
 		dp[1] = 1;
 		dp[2] = 2;
 		dp[3] = 5;
 
+		long mod = 1000000007;
 		for (int i = 4; i <= N; i++) {
 			for (int j = 0; j < i; j++) {
-				dp[i] += dp[j] * dp[i - j - 1];
-				dp[i]%=mod;
+				dp[i] += dp[i - j - 1] * dp[j];
+				dp[i] %= mod;
 			}
 		}
 		System.out.println(dp[N]);

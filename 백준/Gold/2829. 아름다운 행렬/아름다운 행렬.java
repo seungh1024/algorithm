a306f2469207df7fb7c845f9c33f67ba5af8486f
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-26 21:39:29 +0900

    [Gold III] Title: 아름다운 행렬, Time: 440 ms, Memory: 29360 KB -BaekjoonHub

--- 백준/Gold/2829. 아름다운 행렬/아름다운 행렬.java
+++ 백준/Gold/2829. 아름다운 행렬/아름다운 행렬.java
@@ -4,40 +4,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
-
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine().trim());
-
-		int[][] rowSum = new int[N+1][N+2];
-		int[][] colSum = new int[N+1][N+2];
+		int N = Integer.parseInt(br.readLine().trim());
+		int[][] data = new int[N + 2][N + 2];
 		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				rowSum[i][j] = rowSum[i-1][j-1] + num;
-				colSum[i][j] = colSum[i-1][j+1] + num;
+				data[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int[][] A = new int[N + 2][N + 2];
+		int[][] B = new int[N + 2][N + 2];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				A[i][j] += A[i-1][j-1] + data[i][j];
+			}
+			for (int j = N; j > 0; j--) {
+				B[i][j] += B[i -1][j + 1] + data[i][j];
 			}
 		}
 
-		int max = 0;
+		// for (int i = 0; i <= N + 1; i++) {
+		// 	System.out.println(Arrays.toString(A[i]));
+		// }
+		// System.out.println("=====");
+		// for (int i = 0; i <= N + 1; i++) {
+		// 	System.out.println(Arrays.toString(B[i]));
+		// }
 
-		// size
+		int max = Integer.MIN_VALUE;
 		for (int s = 1; s <= N; s++) {
-			int size = N-s;
-			for (int i = 1; i <= s; i++) {
-				for (int j = 1; j <= s; j++) {
-					int A = rowSum[i + size][j + size] - rowSum[i - 1][j - 1];
-					int B = colSum[i + size][j] - colSum[i - 1][j + size + 1];
-					max = Math.max(max, A - B);
+			// System.out.println("s = "+s);
+			for (int i = 1; i + s - 1 <= N; i++) {
+				for (int j = 1; j + s - 1 <= N; j++) {
+					int a = A[i + s - 1][j + s - 1] - A[i-1][j-1];
+					int b = B[i + s - 1][j] - B[i - 1][j + s];
+					int sum =  a - b;
+					// System.out.println("i = " + i + ", j = " + j + ", sum = " + sum + ", a = " + a + ", b= " + b);
+					max = Math.max(max, sum);
 				}
 			}
 		}
 
 		System.out.println(max);
-
-
 	}
 }

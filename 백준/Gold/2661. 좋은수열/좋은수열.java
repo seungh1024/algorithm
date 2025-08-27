2a9e5a58d2f40090024dcdd1877fdf5fd4a48166
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-27 17:13:56 +0900

    [Gold IV] Title: 좋은수열, Time: 128 ms, Memory: 16340 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -4,49 +4,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		find(0, N, "");
+		N = Integer.parseInt(br.readLine());
+
+		String s = "111";
+
+		find(0,"");
 	}
 
-	public static boolean find(int idx, int N, String s) {
-		// System.out.println("idx = "+idx +", s = "+s);
+	public static boolean find(int idx, String s) {
 		if (idx == N) {
 			System.out.println(s);
 			return true;
 		}
 
+		// System.out.println("idx = " + idx + ", s = " + s);
 		for (int i = 1; i <= 3; i++) {
-			// System.out.println("s = "+s +", i = "+i);
-			if (check(s + i)) {
-				if (find(idx + 1, N, s + i)) {
+			String next = s + i;
+			int length = next.length();
+			int range = length/2;
+			boolean flag = false;
+			// System.out.println("range = "+range);
+			for (int j = 1; j <= range; j++) {
+				String sub = next.substring(length - j);
+				// System.out.println("sub = "+sub + ", startswith = "+next.startsWith(sub,length-sub.length()));
+				if (!sub.isEmpty() && next.startsWith(sub, length - sub.length()*2)) {
+					flag = true;
+					break;
+				}
+			}
+			if (!flag) {
+				if (find(idx + 1, next)) {
 					return true;
 				}
 			}
-			// System.out.println("after s =" + s +", i = "+i);
 		}
-
 
 		return false;
-	}
-
-	private static boolean check(String s) {
-		// System.out.println("s = "+s);
-		int length = s.length();
-		int range = length / 2;
-
-		for (int i = 1; i <= range; i++) {
-			String s1 = s.substring(length - i);
-			String s2 = s.substring(length - i * 2, length - i);
-			if (s1.equals(s2)) {
-				// System.out.println("false s = "+s);
-				return false;
-			}
-		}
-
-		// System.out.println("return true");
-
-		return true;
 	}
 }

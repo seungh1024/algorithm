1a6fba96c45bf5b956f5e238be4b355e28f86d6e
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-27 17:54:30 +0900

    [Gold V] Title: 잠입, Time: 352 ms, Memory: 55020 KB -BaekjoonHub

--- 백준/Gold/27113. 잠입/잠입.java
+++ 백준/Gold/27113. 잠입/잠입.java
@@ -6,98 +6,78 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-
 		int now = 1;
-		boolean check = false;
+
+		String result = "YES";
 		for (int i = 1; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			if (x == 1) {
-				int n = Integer.parseInt(st.nextToken());
-				String s = st.nextToken();
+			int v = Integer.parseInt(st.nextToken());
 
-				if (s.equals("R")) {
-					if (now >= n) {
-						check = true;
+			if (v == 1) {
+				int a = Integer.parseInt(st.nextToken());
+				char c = st.nextToken().charAt(0);
+				if (c == 'R') {
+					if (now >= a) {
+						result = "NO";
 						break;
 					}
+					// now = Math.min(now, c - 1);
 				} else {
-					if (n == M) {
-						check = true;
-						break;
-					}
-					now = Math.max(now, n + 1);
+					now = Math.max(now, a + 1);
 				}
-			} else if(x == 2) {
-				int n1 = Integer.parseInt(st.nextToken());
-				String c1 = st.nextToken();
-				int n2 = Integer.parseInt(st.nextToken());
-				String c2 = st.nextToken();
-				if (c1.equals("R")) {
-					if (c2.equals("R")) { // c1 = r, c2 = r
-						int n = Math.min(n1, n2);
-						if (now >= n) {
-							check = true;
-							break;
-						}
-					} else { // c1 = r, c2 = l
-						if (n1 < n2) {
-							if (now >= n1 && now <= n2) {
-								if (n2 + 1 > M) {
-									check = true;
-									break;
-								}
-								now = Math.max(now, n2 + 1);
-							}
-						} else {
-							if (now >= n1 || n1 - n2 == 1) {
-								check = true;
-								break;
-							}
-							now = Math.max(now, n2 + 1);
+			} else if (v == 2) {
+				int a = Integer.parseInt(st.nextToken());
+				char b = st.nextToken().charAt(0);
+				int c = Integer.parseInt(st.nextToken());
+				char d = st.nextToken().charAt(0);
 
+				if (b == 'R' && d == 'L') {
+					if (a < c) {
+						if (now >= a) {
+							now = Math.max(now, c + 1);
+						}
+					} else {
+
+						now = Math.max(now, c + 1);
+
+						if (now >= a) {
+							result = "NO";
+							break;
 						}
 					}
-				} else {
-					if (c2.equals("R")) { // c1 = l, c2 = r
-						if (n1 <= n2) {
-							if (now >= n2 || n2-n1 == 1) {
-								check = true;
-								break;
-							}
-							now = Math.max(now, n1 + 1);
-						} else {
-							if (now >= n2 && now <= n1) {
-								if (n1 + 1 > M) {
-									check = true;
-									break;
-								}
-								now = Math.max(now,n1 + 1);
-							}
-						}
-					} else { // c1 = l , c2 = l
-						int n = Math.max(n1, n2);
-						if (n == M) {
-							check = true;
+				} else if (b == 'R' && d == 'R') {
+					int min = Math.min(a, c);
+					if (now >= min) {
+						result = "NO";
+						break;
+					}
+				} else if (b == 'L' && d == 'L') {
+					int max = Math.max(a, c);
+					now = Math.max(now, max + 1);
+				} else if (b == 'L' && d == 'R') {
+					if (a < c) {
+
+						now = Math.max(now, a + 1);
+						if (now >= c) {
+							result = "NO";
 							break;
 						}
-						now = Math.max(now, n + 1);
+					} else {
+						if (now >= c) {
+							now = Math.max(now, a + 1);
+						}
 					}
 				}
 			}
 
-
-
+			if (now > M) {
+				result = "NO";
+				break;
+			}
 		}
-		if (check) {
-			System.out.println("NO");
-		} else {
-			System.out.println("YES");
-		}
-
+		System.out.println(result);
 	}
 }

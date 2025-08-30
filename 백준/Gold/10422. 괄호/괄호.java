0af306f65920ed368b2a5406bb12560f8ba491b3
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-08-30 18:57:40 +0900

    [Gold IV] Title: 괄호, Time: 144 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -1,35 +1,31 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
 		long[] dp = new long[5001];
 		dp[0] = 1;
 		dp[2] = 1;
+		dp[4] = 2;
+		dp[6] = 5;
+
 		long mod = 1000000007;
-		for (int i = 4; i <= 5000; i += 2) {
-			for (int j = 2; j <= i; j += 2) {
-				dp[i] += dp[j-2] * dp[i-j];
+		for (int i = 8; i <= 5000; i += 2) {
+			for (int j = 0; j < i; j += 2) {
+				dp[i] += (dp[j] * dp[i - j - 2]);
 				dp[i] %= mod;
 			}
 		}
+
 		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			int L = Integer.parseInt(br.readLine());
 			sb.append(dp[L]).append("\n");
 		}
-
 		System.out.println(sb);
-		// (())
-		// ()()
-		//
-		// (())()
-		// ()(())
-		// ()()()
-		// (()())
-		// ((()))
 	}
-
 }

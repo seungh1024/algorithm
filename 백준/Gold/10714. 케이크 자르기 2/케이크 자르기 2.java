6b4792728136d0103fba966b83bbf0aabef896b5
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-03 12:07:22 +0900

    [Gold II] Title: 케이크 자르기 2, Time: 240 ms, Memory: 49072 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10714. 케이크 자르기 2/케이크 자르기 2.java
+++ 백준/Gold/10714. 케이크 자르기 2/케이크 자르기 2.java
@@ -6,27 +6,21 @@ import java.util.*;
 public class Main {
 	public static int N;
 	public static int[] data;
-	public static long[][][] dp;
+	public static long[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N];
-		int[] input = new int[N];
 		for (int i = 0; i < N; i++) {
-			input[i] = Integer.parseInt(br.readLine());
+			data[i] = Integer.parseInt(br.readLine());
 		}
 
 		long result = 0;
+		dp = new long[N][N];
 		for (int i = 0; i < N; i++) {
-			data = new int[N-1];
-			int idx = (i + 1) % N;
-			for(int j = 0; j < N-1; j++){
-				data[j] = input[idx];
-				idx = (idx + 1) % N;
-			}
-			dp = new long[N][N][2];
-			long v = find(0, N - 2, 1) + input[i];
+
+			long v = find((i+1)%N, (i+N - 1)%N, 1)+data[i];
 			result = Math.max(result, v);
 		}
 		System.out.println(result);
@@ -34,26 +28,27 @@ public class Main {
 
 	public static long find(int left, int right, int turn) {
 		// System.out.println("left = "+left + ", right = "+right);
-		if (left > right) {
+		if (left == right) {
+			if(turn == 0)return data[left];
 			return 0;
 		}
 
-		if (dp[left][right][turn] != 0) {
-			return dp[left][right][turn];
+		if (dp[left][right] != 0) {
+			return dp[left][right];
 		}
 
 		long max = 0;
 		int nextTurn = (turn+1)%2;
 		if (turn == 0) {
-			max = Math.max(data[left] + find(left+1, right, nextTurn), data[right] + find(left, right - 1, nextTurn));
+			max = Math.max(data[left] + find((left+1)%N, right, nextTurn), data[right] + find(left, (right +N- 1)%N, nextTurn));
 		} else {
 			if (data[left] > data[right]) {
-				max = find(left + 1, right, nextTurn);
+				max = find((left+1)%N, right, nextTurn);
 			} else {
-				max = find(left, right - 1, nextTurn);
+				max = find(left, (right +N- 1)%N, nextTurn);
 			}
 		}
 
-		return dp[left][right][turn] = max;
+		return dp[left][right] = max;
 	}
 }

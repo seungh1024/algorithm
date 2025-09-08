639b434c9575bbfe246722f61e344fdb6e5a7716
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-08 19:09:18 +0900

    [Gold IV] Title: 토끼가 정보섬에 올라온 이유, Time: 352 ms, Memory: 29104 KB -BaekjoonHub

--- 백준/Gold/17130. 토끼가 정보섬에 올라온 이유/토끼가 정보섬에 올라온 이유.java
+++ 백준/Gold/17130. 토끼가 정보섬에 올라온 이유/토끼가 정보섬에 올라온 이유.java
@@ -6,62 +6,60 @@ import java.util.*;
 public class Main {
 	public static int N, M;
 	public static char[][] data;
-	public static int sx, sy;
-	public static int[] dx = {0, 1, -1};
-	public static int[] dy = {1, 1, 1};
+	public static int[] dx = {1, 0, -1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		data = new char[N][M];
-
-		for (int i = 0; i < N; i++) {
+		data = new char[N + 1][M + 1];
+		int x = 0;
+		int y = 0;
+		for (int i = 1; i <= N; i++) {
 			char[] input = br.readLine().toCharArray();
-			for (int j = 0; j < M; j++) {
-				data[i][j] = input[j];
+			for (int j = 1; j <= M; j++) {
+				data[i][j] = input[j-1];
 				if (data[i][j] == 'R') {
-					data[i][j] = '.';
-					sx = i;
-					sy = j;
+					x= i;
+					y = j;
+
 				}
 			}
 		}
 
-		int[][] dp = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(dp[i], Integer.MIN_VALUE);
+		int[][] dp = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		dp[sx][sy] = 0;
-		for (int j = 0; j < M; j++) {
-			for (int i = 0; i < N; i++) {
-				if (dp[i][j] >= 0) {
-					for (int d = 0; d < 3; d++) {
-						int nx = i + dx[d];
-						int ny = j + dy[d];
-						if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-							if (data[nx][ny] == 'C') {
-								dp[nx][ny] = Math.max(dp[nx][ny],dp[i][j]+1);
-							} else if (data[nx][ny] != '#') {
-								dp[nx][ny] = Math.max(dp[nx][ny], dp[i][j]);
-							}
-						}
+		dp[x][y] = 0;
+		for (int j = 1; j <= M; j++) {
+			for (int i = 1; i <= N; i++) {
+				if (data[i][j] == '#' || dp[i][j] == -1) {
+					continue;
+				}
+				if (data[i][j] == 'C') {
+					dp[i][j]++;
+				}
+				for (int d = 0; d < 3; d++) {
+					int nx = i+dx[d];
+					int ny = j + 1;
+					if (nx > 0 && nx <= N && ny > 0 && ny <= M) {
+						dp[nx][ny] = Math.max(dp[nx][ny], dp[i][j]);
 					}
 				}
 			}
 		}
 
-		int result = -1;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
+		int max = -1;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
 				if (data[i][j] == 'O') {
-
-					result = Math.max(result, dp[i][j]);
+					max = Math.max(max, dp[i][j]);
 				}
 			}
+			// System.out.println(Arrays.toString(dp[i]));
 		}
-
-		System.out.println(result);
+		System.out.println(max);
 	}
 }

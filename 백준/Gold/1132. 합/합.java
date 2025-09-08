173e04327a5b5874b2b5887202c1c39dfada731a
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-08 19:24:52 +0900

    [Gold III] Title: 합, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/1132. 합/합.java
+++ 백준/Gold/1132. 합/합.java
@@ -1,69 +1,63 @@
 
 
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N;
-	public static List<char[]> list;
-	public static char[] al = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-	public static int[] arr;
-	public static boolean[] visited;
-	public static long result = 0;
-	public static Set<Character> set;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		list = new ArrayList<>();
+		int N = Integer.parseInt(br.readLine());
 
-		set = new HashSet<>();
-		long[] count = new long[10];
+		long[] sum = new long[10];
+		Set<Integer> set = new HashSet<>();
+		int cnt = 0;
 		for (int i = 0; i < N; i++) {
-			char[] input = br.readLine().toCharArray();
+			char[] data = br.readLine().toCharArray();
 			long temp = 1;
-			for (int j = input.length - 1; j >= 0; j--) {
-				int idx = input[j]-'A';
-				count[idx] += temp;
+			for (int j = data.length - 1; j >= 0; j--) {
+				int idx = data[j]-'A';
+				if (sum[idx] == 0) {
+					cnt++;
+				}
+				sum[idx] += temp;
 				temp*=10;
 			}
-			set.add(input[0]);
+			set.add(data[0] - 'A');
 		}
 
-		// System.out.println(Arrays.toString(count));
-		PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(o -> o[1]));
+		// System.out.println(Arrays.toString(sum));
+		boolean[] visited = new boolean[10];
+		long min = Long.MAX_VALUE;
+		int idx = 0;
 
-		for (int i = 0; i < 10; i++) {
-			pq.offer(new long[] {i, count[i]});
+		if (cnt == 10) {
+
+			for (int i = 0; i < 10; i++) {
+				if(set.contains(i)) continue;
+				if (sum[i] < min) {
+					min = sum[i];
+					idx = i;
+				}
+			}
+			visited[idx] = true;
 		}
-
 		long result = 0;
-		long num = 0;
-
-		Queue<long[]> q = new ArrayDeque<>();
-		while (!pq.isEmpty()) {
-			long[] now = pq.poll();
-
-			char c = al[(int)now[0]];
-			if (set.contains(c)) {
-				q.offer(now);
-			} else {
-				result += now[1]*num;
-				num++;
-				break;
+		for (int i = 9; i > 0; i--) {
+			long max = Long.MIN_VALUE;
+			for (int j = 0; j < 10; j++) {
+				if(visited[j]) continue;
+				if (sum[j] > max) {
+					max = sum[j];
+					idx = j;
+				}
+			}
+			if (!visited[idx]) {
+				visited[idx] =true;
+				result += (max * i);
 			}
 		}
-
-		while (!q.isEmpty()) {
-			pq.offer(q.poll());
-		}
-		while (!pq.isEmpty()) {
-			long[] now = pq.poll();
-			result += now[1]*num;
-			num++;
-		}
-
 		System.out.println(result);
-	}
 
+	}
 }

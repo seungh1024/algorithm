1b1635d193165026be3a70fe95ed0c52f08ee997
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-09 17:58:53 +0900

    [Gold III] Title: Happy Cow, Time: 156 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Gold/13002. Happy Cow/Happy Cow.java
+++ 백준/Gold/13002. Happy Cow/Happy Cow.java
@@ -5,26 +5,24 @@ import java.util.*;
 
 public class Main {
 	public static int N;
-	public static int[] data;
-	public static int result = 0;
 	public static int[][] dp;
+	public static int[] data;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		data = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		data = new int[N];
 		for (int i = 0; i < N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
 		}
-
 		dp = new int[N][N];
-		result = find(0, N - 1, 1, 0);
 
+		int result = find(0, N-1, 1);
 		System.out.println(result);
 	}
 
-	public static int find(int left, int right, int day, int sum) {
+	public static int find(int left, int right, int day) {
 		if (left > right) {
 			return 0;
 		}
@@ -33,13 +31,10 @@ public class Main {
 			return dp[left][right];
 		}
 
-
 		int max = 0;
-		max = find(left + 1, right, day + 1, sum + data[left] * day) + data[left]*day;
-		max = Math.max(find(left, right - 1, day + 1, sum + data[right] * day)+data[right]*day, max);
+		max = Math.max(find(left + 1, right, day + 1) + data[left] * day,
+			find(left, right - 1, day + 1) + data[right] * day);
 
 		return dp[left][right] = max;
-
-
 	}
 }

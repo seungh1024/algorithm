12e4c0f6117d72b649e21c5d26c50848e7a18121
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-09 20:13:22 +0900

    [Gold I] Title: 타일 밟기, Time: 232 ms, Memory: 93040 KB -BaekjoonHub

--- 백준/Gold/6988. 타일 밟기/타일 밟기.java
+++ 백준/Gold/6988. 타일 밟기/타일 밟기.java
@@ -7,37 +7,38 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] data = new int[N];
+		int[] data = new int[N+1];
+		int[] index = new int[2000000];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int[] temp = new int[1_000_001];
-		Arrays.fill(temp, -1);
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
-			temp[data[i]] = i;
+			index[data[i]] = i;
 		}
-
-		long[][] dp = new long[N][N];
+		long[][] dp = new long[N + 1][N+1];
 
 		long result = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = i + 1; j < N; j++) {
-				int idx = -1;
-				long v = data[i] - (data[j] - data[i]);
-				if (v >= 0) {
-					idx = temp[(int)v];
+		for (int i = 1; i <= N; i++) {
+			int mid = data[i];
+			for (int j = i + 1; j <= N; j++) {
+				int end = data[j];
+				int jump = end-mid;
+				int last = mid-jump;
+				int lastIdx = 0;
+				if (last >= 0) {
+					lastIdx = index[last];
 				}
-				if (idx == -1) {
-					dp[i][j] = data[i] + data[j];
+
+				if (lastIdx == 0) {
+					dp[i][j] = Math.max(dp[i][j], mid + end);
 				} else {
-					dp[i][j] = dp[idx][i] + data[j];
+					dp[i][j] = Math.max(dp[i][j], dp[lastIdx][i] + data[j]);
 					result = Math.max(result, dp[i][j]);
 				}
 			}
 		}
+		// System.out.println(dp[4][5]);
+		// System.out.println(dp[2][1]);
 
 		System.out.println(result);
-
-
 	}
 }

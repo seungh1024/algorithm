4ba0ba652ab093d91a4fedfc2867df48bc32ad81
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-11 17:29:46 +0900

    [Gold III] Title: 학생회 뽑기, Time: 424 ms, Memory: 73440 KB -BaekjoonHub

--- 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.java
+++ 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.java
@@ -4,34 +4,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N, K;
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		List<Integer> list = new ArrayList<>();
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		List<Integer> list = new ArrayList<>(N);
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			int v = Integer.parseInt(st.nextToken());
-			list.add(v);
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
 		}
-		
+
+		int i = 19;
 		int result = 0;
-		for (int i = 19; i >= 0; i--) {
+		while(i>=0){
+			int v = 1<<i;
 			List<Integer> temp = new ArrayList<>();
-			for (int v : list) {
-				if ((v & (1 << i)) != 0) {
-					temp.add(v);
+			for (int j = 0; j < list.size(); j++) {
+				if ((list.get(j) & v) > 0) {
+					temp.add(list.get(j));
 				}
 			}
 
 			if (temp.size() >= K) {
-				result += (1 << i);
-				list= temp;
+				list = temp;
+				result |= v;
 			}
+			i--;
 		}
+
 		System.out.println(result);
 	}
 }

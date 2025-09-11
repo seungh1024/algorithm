fcf5152203fb16992a5fd2c1b8f4b0e867e718ed
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-11 22:47:30 +0900

    [Gold V] Title: 숌 사이 수열, Time: 108 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
+++ 백준/Gold/1469. 숌 사이 수열/숌 사이 수열.java
@@ -6,58 +6,63 @@ import java.util.*;
 public class Main {
 	public static int N;
 	public static int[] data;
-	public static boolean[] visited;
 	public static int[] arr;
-	public static List<String> list;
+	public static boolean[] visited;
+	public static String result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		visited = new boolean[N];
 		for (int i = 0; i < N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(data);
-
 		arr = new int[N * 2];
+		Arrays.sort(data);
 		Arrays.fill(arr, -1);
 
-		if (!find(0)) {
+		visited = new boolean[N];
+		boolean result = find(0, 0);
+		if (result) {
+			StringBuilder sb = new StringBuilder();
+			for (int i : arr) {
+				sb.append(i).append(" ");
+			}
+			System.out.println(sb);
+		} else {
 			System.out.println(-1);
 		}
 	}
 
-	public static boolean find(int idx) {
-		// System.out.println("idx = "+idx + ", arr = "+Arrays.toString(arr));
-		if (idx == 2*N) {
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < 2 * N; i++) {
-				sb.append(arr[i]).append(" ");
-			}
-			System.out.println(sb);
+	public static boolean find(int idx, int arrIdx){
+		if (idx >= N) {
 			return true;
 		}
-
-		if (arr[idx] != -1) {
-			return find(idx + 1);
+		if (arrIdx >= N * 2) {
+			return false;
+		}
+		if(arr[arrIdx] !=-1){
+			if (find(idx, arrIdx + 1)) {
+				return true;
+			}
+			return false;
 		}
 
 		for (int i = 0; i < N; i++) {
-			int v = data[i];
 			if(visited[i]) continue;
-			int r = idx+v+1;
-			if(r >= 2*N || arr[r] != -1) continue;
-			visited[i] = true;
-			arr[idx] = v;
-			arr[r] = v;
-			if (find(idx + 1)) {
-				return true;
+			int v = data[i];
+			if (arrIdx+v+1 < N*2 &&arr[arrIdx] == -1 && arr[arrIdx + v+1] == -1) {
+				arr[arrIdx] = v;
+				arr[arrIdx+v+1] = v;
+				visited[i] = true;
+				if (find(idx + 1, arrIdx + 1)) {
+					return true;
+				}
+				visited[i] = false;
+				arr[arrIdx+v+1] = -1;
+				arr[arrIdx] = -1;
 			}
-			visited[i] = false;
-			arr[idx] = -1;
-			arr[r] = -1;
 		}
 
 		return false;

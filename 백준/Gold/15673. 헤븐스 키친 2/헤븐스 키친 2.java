337a7fd7238972780b940bfd83fc0725aea92ea0
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-13 00:40:34 +0900

    [Gold II] Title: 헤븐스 키친 2, Time: 264 ms, Memory: 29124 KB -BaekjoonHub

--- 백준/Gold/15673. 헤븐스 키친 2/헤븐스 키친 2.java
+++ 백준/Gold/15673. 헤븐스 키친 2/헤븐스 키친 2.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.util.*;
 
@@ -7,48 +6,71 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+
+		int[] data = new int[N + 1];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] data = new int[N+1];
 		for (int i = 1; i <= N; i++) {
 			data[i] = Integer.parseInt(st.nextToken());
 		}
-
 		if (N == 2) {
 			System.out.println(data[1] * data[2]);
 			return;
 		}
 
-		long[] lMin = new long[N + 2];
-		long[] lMax = new long[N + 2];
-		long[] lMinSum = new long[N + 2];
-		long[] lMaxSum = new long[N + 2];
-
+		int[] sumPlusRight = new int[N + 1];
+		int[] sumMinusRight = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			lMin[i] = Math.min(0, lMin[i - 1] + data[i]);
-			lMax[i] = Math.max(0, lMax[i - 1] + data[i]);
-			lMinSum[i] = Math.min(lMinSum[i-1], lMin[i]);
-			lMaxSum[i] = Math.max(lMaxSum[i - 1], lMax[i]);
+			if (sumPlusRight[i - 1] + data[i] > 0) {
+				sumPlusRight[i] += sumPlusRight[i - 1] + data[i];
+			}
+			if (sumMinusRight[i - 1] + data[i] < 0) {
+				sumMinusRight[i] += sumMinusRight[i - 1] + data[i];
+			}
 		}
 
-		long[] rMin = new long[N + 2];
-		long[] rMax = new long[N + 2];
-		long[] rMaxSum = new long[N + 2];
-		long[] rMinSum = new long[N + 2];
+		long[] rightMax = new long[N + 1];
+		long[] rightMin = new long[N + 1];
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int i = 1; i <= N; i++) {
+			max = Math.max(max, sumPlusRight[i]);
+			min = Math.min(min, sumMinusRight[i]);
+			rightMax[i] = max;
+			rightMin[i] = min;
+		}
 
+		int[] sumPlusLeft = new int[N + 2];
+		int[] sumMinusLeft = new int[N + 2];
 		for (int i = N; i > 0; i--) {
-			rMin[i] = Math.min(0, rMin[i + 1] + data[i]);
-			rMax[i] = Math.max(0, rMax[i + 1] + data[i]);
-			rMinSum[i] = Math.min(rMinSum[i + 1], rMin[i]);
-			rMaxSum[i] = Math.max(rMaxSum[i + 1], rMax[i]);
+			if (sumPlusLeft[i + 1] + data[i] > 0) {
+				sumPlusLeft[i] += sumPlusLeft[i + 1] + data[i];
+			}
+			if (sumMinusLeft[i + 1] + data[i] < 0) {
+				sumMinusLeft[i] += sumMinusLeft[i + 1] + data[i];
+			}
 		}
 
+		long[] leftMax = new long[N + 1];
+		long[] leftMin = new long[N + 1];
+		max = Integer.MIN_VALUE;
+		min = Integer.MAX_VALUE;
+		for (int i = N; i > 0; i--) {
+			max = Math.max(max, sumPlusLeft[i]);
+			min = Math.min(min, sumMinusLeft[i]);
+			leftMax[i] = max;
+			leftMin[i] = min;
+		}
 
 		long result = Long.MIN_VALUE;
-		for (int i = 1; i <= N; i++) {
-			result = Math.max(result, lMinSum[i] * rMinSum[i + 1]);
-			result = Math.max(result, lMaxSum[i] * rMaxSum[i + 1]);
+		for (int i = 1; i < N; i++) {
+			result = Math.max(result, rightMax[i] * leftMax[i + 1]);
+			result = Math.max(result, rightMin[i] * leftMin[i + 1]);
 		}
+		// System.out.println(Arrays.toString(rightMax));
+		// System.out.println(Arrays.toString(leftMax));
+		// System.out.println(Arrays.toString(rightMin));
+		// System.out.println(Arrays.toString(leftMin));
 		System.out.println(result);
-
 	}
 }

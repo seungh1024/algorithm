b442bda22ac4e9fcb96bb9b2cbe2e5c8d7d7a517
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-19 00:07:50 +0900

    [Gold IV] Title: 장작 넣기, Time: 120 ms, Memory: 17968 KB -BaekjoonHub

--- 백준/Gold/24884. 장작 넣기/장작 넣기.java
+++ 백준/Gold/24884. 장작 넣기/장작 넣기.java
@@ -1,11 +1,11 @@
 
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	public static int N,W,T, K;
-	public static int[][] dp;
-	public static int[] fire;
+	public static int[] data;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,82 +15,67 @@ public class Main {
 		T = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		dp = new int[T + 1][N + 1]; // 현 시점에서의 위치
-		fire = new int[N + 2];
+		data = new int[N + 2];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
-			fire[i] = Integer.parseInt(st.nextToken());
+			data[i] = Integer.parseInt(st.nextToken());
 		}
-		for (int i = 1; i <= T; i++) {
-			Arrays.fill(dp[i],-1);
-		}
-		int result = find(1, W+1, 0);
+
+		int result = find(W+1, 1, 0);
 		System.out.println(result);
 	}
 
-	public static int find(int t, int idx, int target) {
-		int[] data = getMinusData();
-		int cnt = 0;
+	public static int find(int idx, int time, int save) {
+		int[] count = new int[N + 1];
+
 		for (int i = 1; i <= N; i++) {
-			if(i == target) {
-                if(fire[i]>0) cnt++;
-                continue;
-            }
-			fire[i] -= data[i];
-			if (fire[i] > 0) {
-				cnt++;
+			if(i == save) continue;
+			int cnt = 3;
+			if (data[i - 1] > 0) {
+				cnt--;
 			}
+			if (data[i + 1] > 0) {
+				cnt--;
+			}
+			count[i] = cnt;
 		}
-		if(cnt < K) {
-			for (int i = 1; i <= N; i++) {
-				if(i == target) continue;
-				fire[i] += data[i];
-			}
-			dp[t][idx] = 0;
+
+		int k = changeData(count, -1);
+
+		if (k < K) {
+			changeData(count, 1);
 			return 0;
 		}
 
-		if (t == T) {
-			for (int i = 1; i <= N; i++) {
-				if(i == target) continue;
-				fire[i] += data[i];
-			}
+
+		if (time == T) {
+			changeData(count, 1);
 			return 1;
 		}
 
-		// if (dp[t][idx] > 0) {
-		// 	return dp[t][idx];
-		// }
 		int sum = 0;
-
-		sum += find(t+1,idx,idx);
-		if (idx + 1 <= N) {
-			sum += find(t + 1, idx + 1, idx + 1);
-		}
+		sum += find(idx, time + 1, idx);
 		if (idx - 1 > 0) {
-			sum += find(t + 1, idx - 1, idx - 1);
+			sum += find(idx - 1, time + 1, idx - 1);
+		}
+		if (idx + 1 <= N) {
+			sum += find(idx + 1, time + 1, idx + 1);
 		}
 
-		for (int i = 1; i <= N; i++) {
-			if(i == target) continue;
-			fire[i] += data[i];
-		}
+		changeData(count, 1);
 
-		return dp[t][idx] = sum;
+		return sum;
 	}
 
-	public static int[] getMinusData() {
-		int[] data = new int[N + 1];
+	public static int changeData(int[] count, int m) {
+		int k = 0;
 		for (int i = 1; i <= N; i++) {
-			int cnt = 3;
-			if (fire[i - 1] > 0) {
-				cnt--;
+			data[i] += (count[i]*m);
+			if (data[i] > 0) {
+				k++;
 			}
-			if (fire[i + 1] > 0) {
-				cnt--;
-			}
-			data[i] = cnt;
 		}
-		return data;
+
+		return k;
 	}
 }

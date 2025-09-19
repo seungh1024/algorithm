f4469ff36b71bfc2edaca99b1c5f8f3bd83ee331
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-19 18:03:32 +0900

    [Gold IV] Title: 돌다리 건너기, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/2602. 돌다리 건너기/돌다리 건너기.java
+++ 백준/Gold/2602. 돌다리 건너기/돌다리 건너기.java
@@ -4,54 +4,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static char[] input;
-	public static char[][] data;
+	public static char[] data;
+	public static char[][] bridge;
 	public static int N, M;
 	public static int[][][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		input = br.readLine().toCharArray();
-		char[] arr1 = br.readLine().toCharArray();
-		char[] arr2 = br.readLine().toCharArray();
-		N = input.length;
-		M = arr1.length;
-		data = new char[2][M];
-		data[0] = arr1;
-		data[1] = arr2;
+		data = br.readLine().toCharArray();
+		char[] up = br.readLine().toCharArray();
+		char[] down = br.readLine().toCharArray();
+		N = up.length;
+		M = data.length;
+
+		bridge = new char[2][N];
+		bridge[0] = up;
+		bridge[1] = down;
 
 		dp = new int[2][N][M];
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < N; j++) {
-				Arrays.fill(dp[i][j], -1);
+
+
+
+		for (int i = 0; i < N; i++) {
+			if (data[0] == bridge[0][i]) {
+				dp[0][i][0] = 1;
+			}
+			if (data[0] == bridge[1][i]) {
+				dp[1][i][0] = 1;
+			}
+			for (int j = 1; j < M; j++) {
+				for (int p = 0; p < 2; p++) {
+
+					if (bridge[p][i] == data[j]) {
+						int cnt = 0;
+						int q = (p+1)%2;
+						for (int k = 0; k < i; k++) {
+							cnt += dp[q][k][j - 1];
+						}
+						dp[p][i][j] = cnt;
+					}
+				}
+
+
 			}
 		}
 
-		int sum = find(0, 0, 0);
-		sum += find(1, 0, 0);
-
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += dp[0][i][M - 1];
+			sum += dp[1][i][M - 1];
+		}
 		System.out.println(sum);
-	}
-
-	public static int find(int upDownIdx, int inputIdx, int dataIdx) {
-		if (inputIdx >= N) {
-			return 1;
-		}
-		if (dataIdx >= M) {
-			return 0;
-		}
-
-		if (dp[upDownIdx][inputIdx][dataIdx] != -1) {
-			return dp[upDownIdx][inputIdx][dataIdx];
-		}
-
-		int cnt = 0;
-		for (int i = dataIdx; i < M; i++) {
-			if (data[upDownIdx][i] == input[inputIdx]) {
-				cnt += find((upDownIdx + 1) % 2, inputIdx + 1, i + 1);
-			}
-		}
-
-		return dp[upDownIdx][inputIdx][dataIdx] = cnt;
 	}
 }

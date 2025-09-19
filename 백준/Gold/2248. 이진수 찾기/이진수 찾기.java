6b1c784e67265604cd397c2e3230f1cbe7b88d14
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-20 01:13:41 +0900

    [Gold III] Title: 이진수 찾기, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Gold/2248. 이진수 찾기/이진수 찾기.java
+++ 백준/Gold/2248. 이진수 찾기/이진수 찾기.java
@@ -4,45 +4,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N,L;
-	public static long I;
-
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		I = Long.parseLong(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		long I = Long.parseLong(st.nextToken());
 
-		long[][] dp = new long[32][32];
+		int[][] dp = new int[N+1][N+1];
+
 		dp[0][0] = 1;
-		for (int i = 1; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			dp[i][0] = 1;
-			for (int j = 1; j <=N; j++) {
-				dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-				// dp[i+1][j+1] += dp[i][j];
-				// dp[i+1][j] += dp[i][j];
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] += dp[i-1][j] + dp[i-1][j-1];
 			}
 			// System.out.println(Arrays.toString(dp[i]));
 		}
 
-		String s = "";
+		StringBuilder sb = new StringBuilder();
+
 		for (int i = N-1; i >= 0; i--) {
-			long sum = 0;
+			int sum = 0;
 			for (int j = 0; j <= L; j++) {
 				sum += dp[i][j];
 			}
 
-			if (sum < I) {
-				s += 1;
-				L--;
-				I-=sum;
+			if (sum >= I) {
+				sb.append("0");
 			} else {
-				s+=0;
-
+				I-=sum;
+				L--;
+				sb.append("1");
 			}
 		}
-		System.out.println(s);
+		System.out.println(sb);
 	}
 }

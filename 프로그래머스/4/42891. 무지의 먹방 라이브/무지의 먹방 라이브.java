ca1cd8a7b6ed649304a132735098024d9ad1183b
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-20 00:20:55 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 272.85 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -2,57 +2,69 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] food_times, long k) {
-        int answer = 1;
+        int answer = -1;
+        
         int N = food_times.length;
-        
-        PriorityQueue<Data> pq = new PriorityQueue<>(Comparator.comparingLong((Data o)->o.v).thenComparingInt(o->o.idx));
-        int idx = 0;
-        for(int i : food_times){
-            pq.offer(new Data(idx++,i));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < N; i++){
+            pq.offer(food_times[i]);
+            map.compute(food_times[i],(key,value)->value==null?1:value+1);
         }
-        
-        long minus = 0;
-        long total = N;
+        int total = N;
+        int minus = 0;
         while(!pq.isEmpty()){
-            Data now = pq.poll();
-            
-            long v = now.v-minus;
-            if(v*total <= k){
-                k -= v*total;
-                total--;
-                minus += v;
-            }else{
-                minus += k/total;
-                k = k%total;
+            if(total == 0){
+                return -1;
             }
+            int now = pq.poll();
+            if(now - minus<= 0 || !map.containsKey(now)) continue;
+            long value = now-minus;
+            long v =  value*(long)total;
+            // System.out.println("now = "+value + ", k = "+k + ", v= "+v);
+            if(k < v) {
+                minus += (k/total);
+                k -= (k/total)*total;
+                
+                break;
+            }
+            minus = now;
+            
+            k-=v;
+            
+            int cnt= map.get(now);
+            total -= cnt;
+            map.remove(now);
         }
         
+        Queue<int[]> q = new ArrayDeque<>();
         for(int i = 0; i < N; i++){
             food_times[i] -= minus;
-            if(food_times[i]>0 && k >= 0){
-                
-                answer = i+1;
-                k--;
+            if(food_times[i] > 0){
+                q.offer(new int[]{i,food_times[i]});
             }
         }
-        if(k >= 0){
-            return -1;
+        // System.out.println(Arrays.toString(food_times));
+        // System.out.println("k = "+k);
+        
+        while(k>0&&!q.isEmpty()){
+            int[] now = q.poll();
+            now[1]--;
+            k--;
+            if(now[1] > 0){
+                q.offer(now);
+            }
+            
         }
+        
+        if(!q.isEmpty()){
+            answer = q.poll()[0];
+        }
+        if(answer != -1){
+            answer++;
+        }
+        
         
         return answer;
-    }
-    
-    public static class Data{
-        int idx;
-        long v;
-        
-        public Data(int idx, long v){
-            this.idx = idx;
-            this.v = v;
-        }
-        
-        public String toString(){
-            return "idx = "+idx + ", v= "+v;
-        }
     }
 }

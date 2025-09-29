557b9223c677311bec75364ab51c16287bbf923d
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-29 17:26:25 +0900

    [Gold V] Title: 흙길 보수하기, Time: 216 ms, Memory: 20120 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -10,52 +10,29 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
 
-		PriorityQueue<Data> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.from));
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int cnt = (to - from + (L - 1)) / L;
-			pq.offer(new Data(from, to, cnt));
+			pq.offer(new int[]{from, to});
 		}
 
-
-		int result = 0;
 		int point = 0;
+		int cnt = 0;
+
 		while (!pq.isEmpty()) {
-			Data now = pq.poll();
-			int left = Math.max(point, now.from);
-			int right = Math.max(point, now.to);
-			if (right > left) {
-				int cnt = (right - left + (L - 1)) / L;
-				result += cnt;
-				int size = cnt*L;
-				point = left+size;
+			int[] now = pq.poll();
+			if (now[0] > point) {
+				point = now[0];
 			}
+
+			int range = now[1]-point;
+			int plus = (range + L - 1) / L;
+			cnt += plus;
+			point += (plus * L);
 		}
-
-		System.out.println(result);
-
-	}
-
-	public static class Data{
-		int from;
-		int to;
-		int cnt;
-
-		@Override
-		public String toString() {
-			return "Data{" +
-				"from=" + from +
-				", to=" + to +
-				", cnt=" + cnt +
-				'}';
-		}
-
-		public Data(int from, int to, int cnt) {
-			this.from = from;
-			this.to = to;
-			this.cnt = cnt;
-		}
+		System.out.println(cnt);
 	}
 }

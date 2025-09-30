376327dae524b1f082eff1a5e1a129dbc02d4937
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-30 18:02:29 +0900

    [Gold IV] Title: 문자열 폭발, Time: 444 ms, Memory: 78104 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -4,58 +4,81 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Deque<Character> q = new ArrayDeque<>();
-		char[] input = br.readLine().toCharArray();
+		Queue<Character> q = new ArrayDeque<>();
+		char[] input =br.readLine().toCharArray();
 		for (char c : input) {
 			q.offer(c);
 		}
-
-		char[] data = br.readLine().toCharArray();
-
-		Deque<Character> q2 = new ArrayDeque<>();
-		int idx = 0;
+		char[] str = br.readLine().toCharArray();
+		Deque<Data> dq = new ArrayDeque<>();
 		while (!q.isEmpty()) {
-			char now = q.poll();
-			if (now == data[idx]) {
-				idx++;
-			} else {
-				idx = 0;
-				if (now == data[idx]) {
-					idx++;
+			char c = q.poll();
+			if (!dq.isEmpty()) {
+				Data peek = dq.peekLast();
+				int idx = peek.idx + 1;
+				if (peek.idx == -1) {
+					peek.idx = -1;
 				}
-			}
-			q2.offer(now);
+				if (idx != -1 && idx < str.length && str[idx] == c) {
+					if (idx == str.length - 1) {
+						for (int i = peek.idx; i >= 0; i--) {
 
-			// System.out.println("q2 = "+q2);
-			// System.out.println("q = "+q);
-			// System.out.println("idx = "+idx);
-			if (idx == data.length) {
-				for (int i = 0; i < idx; i++) {
-					q2.pollLast();
-				}
-				while (!q2.isEmpty()) {
-					char last = q2.pollLast();
-					q.offerFirst(last);
-					if (last == data[0]) {
-						break;
+							dq.pollLast();
+						}
+
+					} else {
+						dq.offer(new Data(c, idx));
+					}
+				} else {
+					if (c == str[0]) {
+						if (str.length != 1) {
+							dq.offer(new Data(c, 0));
+						}
+					} else {
+						dq.offer(new Data(c, -1));
 					}
 				}
-
-				idx = 0;
+			} else {
+				if (c == str[0]) {
+					if (str.length != 1) {
+						dq.offer(new Data(c, 0));
+					}
+				} else {
+					dq.offer(new Data(c, -1));
+				}
 			}
 		}
+		// System.out.println(dq);
 
-		StringBuilder sb = new StringBuilder();
-		while (!q2.isEmpty()) {
-			sb.append(q2.poll());
+		if (!dq.isEmpty()) {
+			StringBuilder sb = new StringBuilder();
+			while (!dq.isEmpty()) {
+				sb.append(dq.poll().c);
+			}
+			System.out.println(sb);
+		} else {
+			System.out.println("FRULA");
 		}
-		if (sb.isEmpty()) {
-			sb.append("FRULA");
-		}
-		System.out.println(sb);
+	}
 
+
+	public static class Data{
+		char c;
+		int idx;
+
+		public Data(char c, int idx) {
+			this.c = c;
+			this.idx = idx;
+		}
+
+		@Override
+		public String toString() {
+			return "Data{" +
+				"c=" + c +
+				", idx=" + idx +
+				'}';
+		}
 	}
 }

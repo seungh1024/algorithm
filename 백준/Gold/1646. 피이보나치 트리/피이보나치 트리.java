8156ce28125e4e41968baf0543a44e4939d9e996
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-09-30 16:31:36 +0900

    [Gold I] Title: 피이보나치 트리, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1646. 피이보나치 트리/피이보나치 트리.java
+++ 백준/Gold/1646. 피이보나치 트리/피이보나치 트리.java
@@ -5,66 +5,77 @@ import java.util.*;
 
 public class Main {
 	public static long[] dp;
+	public static int N,start, end;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int from = Integer.parseInt(st.nextToken());
-		int to = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+
 
 		dp = new long[51];
 		dp[0] = 1;
-		dp[1] = 1;
+		dp[1] =1;
 		for (int i = 2; i <= 50; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2]+1;
+			dp[i] = dp[i - 1] + dp[i - 2] + 1;
 		}
+		// System.out.println(dp[50]);
 
-		List<Character> l1 = new ArrayList<>();
-		List<Character> l2 = new ArrayList<>();
-		find(N, from, l1);
-		find(N, to, l2);
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
 
-		int size = Math.min(l1.size(), l2.size());
+		find(N, start, sb1);
+		find(N, end, sb2);
+		String s1 = sb1.toString();
+		String s2 = sb2.toString();
+
+		int length1 = s1.length();
+		int length2 = s2.length();
+
 		int idx = 0;
-		for (int i = 0; i < size; i++) {
-			if (l1.get(i) != l2.get(i)) {
+		int range = Math.min(length1, length2);
+		for (int i = 0; i < range; i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
 				break;
 			}
 			idx++;
 		}
+
 		StringBuilder sb = new StringBuilder();
-		if (idx == l1.size()) {
-			for (int i = idx; i < l2.size(); i++) {
-				sb.append(l2.get(i));
+		if (idx == length1) {
+			for (int i = length1; i < length2; i++) {
+				sb.append(s2.charAt(i));
 			}
-		} else if (idx == l2.size()) {
-			for (int i = idx; i < l1.size(); i++) {
+		} else if (idx == length2) {
+			for (int i = length2; i < length1; i++) {
 				sb.append('U');
 			}
 		} else {
-			for (int i = idx; i < l1.size(); i++) {
+			for (int i = idx; i < length1; i++) {
 				sb.append('U');
 			}
-			for (int i = idx; i < l2.size(); i++) {
-				sb.append(l2.get(i));
+			for (int i = idx; i < length2; i++) {
+				sb.append(s2.charAt(i));
 			}
+
 		}
+
+
 		System.out.println(sb);
 	}
 
-	public static void find(int idx, long value, List list) {
-		if (idx <= 1 || value == 1) {
-			return;
-		}
-		value--;
-
-		if (dp[idx - 2] >= value) {
-			list.add('L');
-			find(idx - 2, value, list);
+	public static void find(int idx, long num, StringBuilder s) {
+		if(idx <= 1 || num == 1) return;
+		num--;
+		if (dp[idx - 2] >= num) {
+			s.append('L');
+			find(idx - 2, num, s);
 		} else {
-			list.add('R');
-			find(idx - 1, value - dp[idx - 2], list);
+			s.append('R');
+			find(idx - 1, num - dp[idx - 2], s);
 		}
+
 	}
 }

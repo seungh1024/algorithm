09fab7a71656390423443b803c68facf13ecc192
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-10-08 21:44:15 +0900

    [Gold III] Title: 벌레컷, Time: 560 ms, Memory: 94524 KB -BaekjoonHub

--- 백준/Gold/27651. 벌레컷/벌레컷.java
+++ 백준/Gold/27651. 벌레컷/벌레컷.java
@@ -4,48 +4,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int N;
+	public static long[] sum;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		long[] data = new long[N+1];
+		N = Integer.parseInt(br.readLine());
+		sum = new long[N + 1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			data[N-i] = Long.parseLong(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			sum[i] += sum[i-1]+input;
 		}
 
-		long[] sum = new long[N + 1];
-		for (int i = 1; i <= N; i++) {
-			sum[i] += sum[i - 1] + data[i];
+		long result = 0;
+		for (int i = 1; i < N; i++) {
+			long head = sum[i];
+			result += find(i, head);
 		}
-		// System.out.println(Arrays.toString(sum));
-
-		long cnt = 0;
-		for (int i = 1; i <= N; i++) {
-			int result = find(i + 1, sum[i], sum);
-			cnt += result;
-		}
-		System.out.println(cnt);
+		System.out.println(result);
 	}
 
-	public static int find(int start, long target, long[] sum) {
-		int end = sum.length - 1;
-
-		int idx = start-1;
-		int N = end;
-		if (target >= sum[N] - sum[idx]) {
-			return 0;
-		}
+	public static long find(int s, long head) {
+		int start = s+1;
+		int end = N;
 
 		while (start < end) {
 			int mid = (start + end) / 2;
-			if (sum[mid] - sum[idx] > target && sum[N] - sum[mid] < target) {
+
+			long chest = sum[mid]-sum[s];
+			long stomach = sum[N] - sum[mid];
+			// System.out.println("mid = "+mid +", head = "+head + ", chest = "+chest +", stomach = "+stomach);
+			if (chest > stomach) {
 				end = mid;
 			} else {
-				start = mid+1;
+				start = mid + 1;
 			}
 		}
+		int left = start;
+		// System.out.println("left = "+left + ", head = "+head + ", chest = "+(sum[left]-sum[s]));
 
-		return N-start;
+		if (left == N || sum[N] - sum[left] <=head) {
+			return 0;
+		}
+
+		start = left;
+		end = N-1;
+		long chest = sum[left]-sum[s];
+		while (start < end) {
+			int mid = (start + end) / 2;
+
+			long stomach = sum[N] - sum[mid];
+			// System.out.println("mid = "+mid + ", chest = "+chest + ", stomach = "+stomach);
+			if (head >= stomach) {
+				end = mid;
+			} else {
+				start = mid + 1;
+			}
+		}
+		int right = start;
+
+		if ( sum[N] - sum[right] <= head) {
+			right--;
+		}
+		if (sum[N] - sum[right] >= sum[right] - sum[s]) {
+			return 0;
+		}
+
+
+		// System.out.println("i = "+ s +", head = "+head + ", left = " + left + ", right = " + right);
+		return right-left+1;
 	}
-
 }
+
+// 1, 12, 2
+// 1, 9, 5
+// 4, 6, 5

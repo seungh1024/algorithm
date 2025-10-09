9549350139d3e1767ed1201d5cb17997ee2e6294
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-10-09 20:37:12 +0900

    [Gold III] Title: 승형이의 사탕 사기, Time: 184 ms, Memory: 15016 KB -BaekjoonHub

--- 백준/Gold/28071. 승형이의 사탕 사기/승형이의 사탕 사기.java
+++ 백준/Gold/28071. 승형이의 사탕 사기/승형이의 사탕 사기.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class Main {
 	public static int N,M, K;
-	public static int[] count;
+	public static int[] data;
+	public static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,33 +14,39 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		count = new int[N];
+
+		data = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			count[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			data[i] = Integer.parseInt(st.nextToken());
 		}
-		int[] dp = new int[90001];
-		Arrays.fill(dp, Integer.MAX_VALUE);
+
+		dp = new int[90001];
+
+		Arrays.fill(dp,Integer.MAX_VALUE);
 		dp[0] = 0;
-		for (int i = 1; i <= 90000; i++) {
-			for (int j = 0; j < N; j++) {
-				int cnt = count[j];
-				if (i - cnt >= 0 && dp[i - cnt] != Integer.MAX_VALUE) {
-					dp[i] = Math.min(dp[i], dp[i - cnt] + 1);
+
+		for (int i = 1; i <= N; i++) {
+			int v = data[i];
+			for (int j = v; j <=90000; j++) {
+				if (dp[j - v] != Integer.MAX_VALUE) {
+					dp[j] = Math.min(dp[j], dp[j - v] + 1);
 				}
 			}
 		}
 
 
 		int result = 0;
-		for (int i = 90000; i >= 0; i--) {
-			if (i%K == 0 && dp[i] <= M) {
-				result = i;
+
+		for (int j = 90000; j >= 0; j--) {
+			if (dp[j] <= M && j % K == 0) {
+				result = Math.max(result, j);
 				break;
 			}
 		}
+
+
 		System.out.println(result);
-
-
 	}
+
 }

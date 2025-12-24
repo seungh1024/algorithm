68f1a7935113cb6671a7be8b4b3344b07f8dcf07
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2025-12-24 17:14:26 +0900

    [Gold III] Title: 🧩 N-Queen (Easy), Time: 248 ms, Memory: 16812 KB -BaekjoonHub

--- 백준/Gold/30242. 🧩 N－Queen （Easy）/🧩 N－Queen （Easy）.java
+++ 백준/Gold/30242. 🧩 N－Queen （Easy）/🧩 N－Queen （Easy）.java
@@ -5,28 +5,21 @@ import java.util.*;
 
 public class Main {
 	public static int N;
-	public static int[] visited;
+	public static int[] data;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-
+		data = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		visited = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			int idx = Integer.parseInt(st.nextToken());
-			if (idx != 0) {
-				visited[i] = idx;
-			}
+			data[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// System.out.println(Arrays.toString(visited));
-		boolean result = find(1);
-		// System.out.println(Arrays.toString(visited));
-		if (result) {
+		if (find(1)) {
 			StringBuilder sb = new StringBuilder();
 			for (int i = 1; i <= N; i++) {
-				sb.append(visited[i]).append(" ");
+				sb.append(data[i]).append(" ");
 			}
 			System.out.println(sb);
 		} else {
@@ -34,39 +27,49 @@ public class Main {
 		}
 	}
 
-	public static boolean find(int row) {
-		// System.out.println("row = "+row);
-		if (row == N+1) {
+	public static boolean find(int x) {
+		if (x == N+1) {
 			return true;
 		}
 
-		if (visited[row] > 0) {
-			if (find(row + 1)) {
+		if (data[x] != 0) {
+			if (find(x + 1)) {
 				return true;
 			}
-		} else {
+			return false;
+		}
 
-			for (int j = 1; j <= N; j++) {
-				boolean check = false;
-				for (int r = 1; r <= N; r++) {
-					int idx = visited[r];
-					if(idx == 0)continue;
-					double temp = Math.abs((double)(j-idx)/(double)(row-r));
-					if (temp == 1.0 || j == idx) {
-						check = true;
-						break;
-					}
+		for (int y = 1; y <= N; y++) {
+			boolean check = false;
+			// if (data[1] == 3 && data[2] == 1 && x == 3 && y == 6) {
+			// 	System.out.println("here");
+			// }
+			for (int lx = 1; lx <=N; lx++) {
+				int ly = data[lx];
+				if(x == lx || ly == 0) continue;
+				if(ly == y){
+					check = true;
+					break;
 				}
-
-				if(check) continue;
-
-				visited[row] = j;
-				if (find(row + 1)) {
+				// if (data[1] == 3 && data[2] == 1 && x == 3 && y == 6) {
+				// 	System.out.println("x = "+x +", y ="+y);
+				// 	System.out.println("lx = "+lx+", ly = "+ly);
+				// }
+				double v = (double)Math.abs(y - ly) / (double)Math.abs(x - lx);
+				if (v == 1) {
+					check = true;
+					break;
+				}
+			}
+			if (!check) {
+				data[x] = y;
+				if (find(x + 1)) {
 					return true;
 				}
-				visited[row] = 0;
+				data[x] = 0;
 			}
 		}
+
 
 		return false;
 	}

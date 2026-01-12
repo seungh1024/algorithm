119bb5232c036c7a6600c83c49d5758ae7e38a3c
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-01-12 18:21:39 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 436 ms, Memory: 60460 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
 	public static int N;
 	public static int[][] data;
-	public static int[][] visited;
+	public static int[][] dp;
 	public static int[] dx = {0, 0, 1, -1};
 	public static int[] dy = {1, -1, 0, 0};
 
@@ -14,48 +14,46 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		data = new int[N][N];
-		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				data[i][j] = Integer.parseInt(st.nextToken());
-				pq.offer(new int[] {i, j, data[i][j]});
 			}
 		}
 
 		int result = 0;
-		visited = new int[N][N];
+		dp = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(dp[i], -1);
+		}
 
-		while (!pq.isEmpty()) {
-			int[] now = pq.poll();
-			if(visited[now[0]][now[1]] != 0) continue;
-			result = Math.max(result, findBig(now[0], now[1]));
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(dp[i][j] != -1) continue;
+				// System.out.println("======" + ", i = "+i +", j = "+j);
+				result = Math.max(result, find(i, j, 1));
+			}
 		}
 
 		System.out.println(result);
 	}
 
-	public static int findBig(int x, int y) {
-		PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt((int[] o)->o[2]));
-		q.offer(new int[]{x,y,data[x][y],1});
-		visited[x][y] = 1;
+	public static int find(int x, int y, int cnt) {
+		// System.out.println("x = "+x + ", y = "+y + ", cnt = "+cnt +", dp = "+dp[x][y]);
+		if(dp[x][y] != -1) return dp[x][y];
 
-		int cnt = 0;
-		while (!q.isEmpty()) {
-			int[] now = q.poll();
-			cnt = Math.max(cnt, now[3]);
-			for (int d = 0; d < 4; d++) {
-				int nx = now[0]+dx[d];
-				int ny = now[1]+dy[d];
-
-				if (nx >= 0 && nx < N && ny >= 0 && ny < N && now[2] < data[nx][ny] && visited[nx][ny] < now[3] + 1) {
-					visited[nx][ny] = now[3]+1;
-					q.offer(new int[] {nx, ny, data[nx][ny], visited[nx][ny]});
-				}
+		int max = 1;
+		for (int d = 0; d < 4; d++) {
+			int nx = x+dx[d];
+			int ny = y+dy[d];
+			if (nx >= 0 && nx < N && ny >= 0 && ny < N && data[nx][ny] > data[x][y]) {
+				max = Math.max(max, find(nx, ny, cnt + 1)+1);
 			}
 		}
 
-		return cnt;
+		return dp[x][y] = max;
+
+
 	}
 
 }

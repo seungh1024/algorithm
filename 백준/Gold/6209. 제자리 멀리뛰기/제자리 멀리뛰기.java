8f48aba5650e009d2d823217573da5e178489316
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-02-09 19:17:08 +0900

    [Gold II] Title: 제자리 멀리뛰기, Time: 256 ms, Memory: 21916 KB -BaekjoonHub

--- 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.java
+++ 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.java
@@ -1,61 +1,87 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int D, N, M;
+	public static int D,N, M;
 	public static int[] data;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		D = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		data = new int[N+2];
-		for (int i = 1; i <= N; i++) {
-			data[i] = Integer.parseInt(br.readLine());
+
+		for(int i=1;i<=N;i++) {
+			int num = Integer.parseInt(br.readLine());
+			data[i] = num;
 		}
 		data[N+1] = D;
 		Arrays.sort(data);
 
-		find();
-	}
-
-	public static void find() {
-		int start = 0;
-		int end = D;
-
-		int result = 0;
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			// System.out.println("mid = "+mid);
-			if (!check(mid)) {
-				end = mid-1;
-			} else {
-				result = mid;
-				start = mid + 1;
-			}
-		}
-
-		// System.out.println(Arrays.toString(data));
+		int result = find();
 		System.out.println(result);
 	}
 
-	public static boolean check(int jump) {
-		int cnt = 0;
-		int now = 0;
-		for (int i = 1; i <= N + 1; i++) {
-			if (data[i] - data[now] < jump) {
-				cnt++;
+	public static int find() {
+		int start = 0;
+		int end = 1_000_000_000;
+
+		while (start < end) {
+			int mid = (start + end) / 2;
+
+			if (!check(mid)) {
+				end = mid;
 			} else {
-				now = i;
+				start = mid+1;
 			}
 		}
 
-		if (cnt > M) {
-			return false;
+		if (!check(start)) {
+			start--;
+		}
+
+		return start;
+	}
+
+	public static boolean check(int jump) {
+		int left = 0;
+		int m = M;
+		for (int right = 1; right <= N + 1; right++) {
+			if (data[right] - data[left] < jump) {
+				m--;
+			} else {
+				left = right;
+			}
+			if (m < 0) {
+				return false;
+			}
 		}
 
 		return true;
+	}
+
+	public static class Data{
+		int left;
+		int right;
+		int value;
+
+		@Override
+		public String toString() {
+			return "Data{" +
+				"left=" + left +
+				", right=" + right +
+				", value=" + value +
+				'}';
+		}
+
+		public Data(int left, int right, int value) {
+			this.left = left;
+			this.right = right;
+			this.value = value;
+		}
 	}
 }

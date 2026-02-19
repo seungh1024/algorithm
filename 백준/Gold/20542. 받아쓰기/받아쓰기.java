847f64f3565d357b4520ed19053a4b804e95af88
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-02-19 17:26:10 +0900

    [Gold III] Title: 받아쓰기, Time: 332 ms, Memory: 88392 KB -BaekjoonHub

--- 백준/Gold/20542. 받아쓰기/받아쓰기.java
+++ 백준/Gold/20542. 받아쓰기/받아쓰기.java
@@ -1,46 +1,51 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int N, M;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		char[] data1 = br.readLine().toCharArray();
 		char[] data2 = br.readLine().toCharArray();
 
 		int[][] dp = new int[N + 1][M + 1];
+
 		for (int i = 1; i <= N; i++) {
 			dp[i][0] = i;
 		}
 		for (int j = 1; j <= M; j++) {
 			dp[0][j] = j;
-
 		}
+
 
 		for (int i = 1; i <= N; i++) {
-			char a = data1[i-1];
+			char c1 = data1[i-1];
 			for (int j = 1; j <= M; j++) {
-				char b = data2[j - 1];
-				if (dp[i][j] == 0) {
-					dp[i][j] = Integer.MAX_VALUE;
+				char c2 = data2[j-1];
+				if (c1 == 'i' && (c2 == 'j' || c2 == 'l')) {
+					c2 = 'i';
+				} else if (c1 == 'v' && c2 == 'w') {
+					c2 = 'v';
 				}
-				if (a == 'i' && (b == 'j' || b == 'l')) {
-					b = 'i';
-				} else if (a == 'v' && b == 'w') {
-					b = 'v';
-				}
-				if (a == b) {
-					dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+
+				if (c1 == c2) {
+					dp[i][j] = dp[i - 1][j - 1];
 				} else {
-					dp[i][j] = Math.min(dp[i][j], Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j]))) + 1;
+					int min = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+					dp[i][j] = min + 1;
 				}
 			}
+			// System.out.println("i= " + i);
+			// System.out.println(Arrays.toString(dp[i]));
 		}
+
 		System.out.println(dp[N][M]);
+
+
 	}
 }

6cc8b34a3d8db527eceab3955bc74f2d330b1b5b
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-03-02 01:27:28 +0900

    [Gold II] Title: 기업투자, Time: 152 ms, Memory: 15192 KB -BaekjoonHub

--- 백준/Gold/2662. 기업투자/기업투자.java
+++ 백준/Gold/2662. 기업투자/기업투자.java
@@ -9,60 +9,60 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[][] data = new int[M+1][N+1];
 
+		int[][] data = new int[M + 1][N + 1];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
+			st.nextToken();
 			for (int j = 1; j <= M; j++) {
 				data[j][i] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		int[][] dp = new int[M+1][N + 1];
+		int[][] dp = new int[M + 1][N + 1];
 		int[][] check = new int[M + 1][N + 1];
-		for (int i = 1; i <= M; i++) {
-			for (int j = 1; j <= N; j++) {
+
+		for (int i = 1; i <= M; i++) { // 기업
+			for (int j = 1; j <= N; j++) { // 투자금액
 				int v = data[i][j];
-				if (dp[i - 1][j] < v) {
+				if (dp[i - 1][j] < v) { // 이전 기업에서의 j만큼 투자한 것보다 v가 더 크면 현재 값은 v로
+					dp[i][j] = v;
 					check[i][j] = j;
+				} else {
+					dp[i][j] = dp[i-1][j];
 				}
-				dp[i][j] = Math.max(dp[i - 1][j], v);
+
+				// k라는 금액을 써서 이전 값에서 더했을 때 더 큰 값이 있는지 확인
 				for (int k = 1; k < j; k++) {
-					int now = data[i][k];
-					if (dp[i][j] < dp[i - 1][j - k] + now) {
-						dp[i][j] = dp[i-1][j-k]+now;
+					int c = data[i][k];
+					if (dp[i][j] < dp[i - 1][j - k] + c) {
+						dp[i][j] = dp[i - 1][j - k] + c;
 						check[i][j] = k;
 					}
 				}
 			}
-			// System.out.println(Arrays.toString(dp[i]));
 		}
-		System.out.println(dp[M][N]);
 
-		StringBuilder sb = new StringBuilder();
 		Stack<Integer> stack = new Stack<>();
-
-		// System.out.println("====");
-		// for (int i = 1; i <= M; i++) {
-		// 	System.out.println(Arrays.toString(check[i]));
-		// }
-
 		int v = dp[M][N];
 		for (int i = M; i > 0; i--) {
 			int target = 0;
-			for (int j = N; j > 0; j--) {
+			for (int j = 1; j <= N; j++) {
 				if (dp[i][j] == v) {
 					target = check[i][j];
 					break;
 				}
 			}
-			v-= data[i][target];
+			v -= data[i][target];
 			stack.push(target);
 		}
 
+		StringBuilder sb = new StringBuilder();
+		sb.append(dp[M][N]).append("\n");
+		
 		while (!stack.isEmpty()) {
-			sb.append(stack.pop()).append(" ");
+			int money = stack.pop();
+			sb.append(money).append(" ");
 		}
 		System.out.println(sb);
 	}

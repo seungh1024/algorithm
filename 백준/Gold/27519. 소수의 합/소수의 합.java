965951c29a4fdc6894cdf4b892d41505c263c8e9
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-03-02 16:32:33 +0900

    [Gold II] Title: 소수의 합, Time: 2720 ms, Memory: 17940 KB -BaekjoonHub

--- 백준/Gold/27519. 소수의 합/소수의 합.java
+++ 백준/Gold/27519. 소수의 합/소수의 합.java
@@ -5,46 +5,62 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		int size = 100001;
+		int[] dp = init(size);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = 100000;
-		int[] dp = new int[size + 1];
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int t = 0; t < T; t++) {
+			int n = Integer.parseInt(br.readLine());
+			sb.append(dp[n]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	public static int[] init(int size) {
+		int mod = 1000000007;
+		int[] dp = new int[size];
 		List<Integer> list = new ArrayList<>();
-		boolean[] visited = new boolean[size + 1];
+		boolean[] visited = new boolean[size];
 		int range = (int)Math.sqrt(size);
 		for (int i = 2; i <= range; i++) {
 			int num = i;
-			while (num <= size) {
+			while (num < size) {
 				num += i;
-				if(num > size) break;
+				if(num >=size) break;
 				visited[num] = true;
 			}
 		}
 
-		for (int i = 2; i <= size; i++) {
+		for (int i = 2; i < size; i++) {
 			if (!visited[i]) {
 				list.add(i);
 			}
 		}
 
-		// System.out.println(list);
-		// System.out.println(list.size());
-
-		int mod = 1000000007;
 		for (int i : list) {
 			dp[i] += 1;
-			for (int j = 2; j <= size; j++) {
-				if(j-i < 0) continue;
-				dp[j] += dp[j-i];
+			for (int j = 2; j < size; j++) {
+				if(j-i <0) continue;
+				dp[j] += dp[j - i];
 				dp[j] %= mod;
 			}
 		}
 
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			sb.append(dp[N]).append("\n");
-		}
-		System.out.println(sb);
+
+
+		return dp;
 	}
 }
+/**
+ * 1 -> 0
+ * 2 -> 1
+ * 3 -> 1
+ * 4 -> 1
+ * 5 -> 2
+ * 6 -> 2  (3+3), (2+2+2),
+ * 7 -> 3
+ * 8 -> 3 (2+2+2+2), (2+3+3), (3+5)
+ * 9 -> 4 (3+3+3), (2+2+2+3), (2+2+5), (2+7)
+ * 10 -> 5
+ */

735972fc8cdb7fd36eeaec7adeb3372629e8d6f6
seunghun kang <77014020+seungh1024@users.noreply.github.com>
2026-03-10 19:11:00 +0900

    [Gold II] Title: 집합의 개수, Time: 256 ms, Memory: 18680 KB -BaekjoonHub

--- 백준/Gold/2092. 집합의 개수/집합의 개수.java
+++ 백준/Gold/2092. 집합의 개수/집합의 개수.java
@@ -4,47 +4,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	public static int T,A,S, B;
+	public static int[] count;
+	public static List<Integer> list;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		int A = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
+		A = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
 
-		int[] count = new int[T + 1];
-		int[] nums = new int[A + 1];
+		list = new ArrayList<>();
+		count = new int[4001];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= A; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-			count[nums[i]]++;
+		for (int i = 0; i < A; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (count[num] == 0) {
+				list.add(num);
+			}
+			count[num]++;
 		}
 
-		int mod = 1_000_000;
-		int[][] dp = new int[T+1][A + 1];
+		int[][] dp = new int[T + 1][A+1];
+		int mod = 1000000;
 		for (int i = 1; i <= T; i++) {
-
-			// 어떤 숫자를 그 개수만큼 고를 때의 경우의 수는 기본적으로 1임. 초기화를 한다.
 			for (int j = 1; j <= count[i]; j++) {
-				dp[i][j] ++;
+				dp[i][j] += 1;
 			}
 
 			for (int j = 1; j <= A; j++) {
-				dp[i][j] += dp[i - 1][j]; // i번째 선택 안하고 i-1번째의 결과를 그대로 가져오는 경우.
+				dp[i][j] += dp[i - 1][j];
 				for (int k = 1; k <= count[i]; k++) {
 					if (j - k > 0) {
-						dp[i][j] += dp[i-1][j-k];
+						dp[i][j] += dp[i - 1][j - k];
 						dp[i][j] %= mod;
 					}
 				}
 			}
 		}
-
+		
 		int result = 0;
 		for (int j = S; j <= B; j++) {
 			result += dp[T][j];
 			result %= mod;
 		}
 		System.out.println(result);
+
 	}
 }
